fa53aac79994d916357be9a0542e2def5285ca0e
Mircea Markus <mircea.markus@gmail.com>
2009-11-04 16:54:03 +0200

    added javadoc

--- framework/src/main/java/org/cachebench/fwk/DistStage.java
+++ framework/src/main/java/org/cachebench/fwk/DistStage.java
@@ -7,20 +7,37 @@ import java.io.Serializable;
 import java.util.List;
 
 /**
- * // TODO: Mircea - Document this!
+ * Defines an stage that will be run on both server and nodes.
  *
- * @author
+ * @author Mircea.Markus@jboss.com
  */
 public interface DistStage extends Stage, Serializable, Cloneable {
 
+   /**
+    * Called on server. Server state should not be passed to the slaves.
+    */
    public void initOnServer(ServerState serverState);
 
+   /**
+    * Before sending the state to the slave, this will be called to set salve's index.
+    */
    public void setNodeIndex(int nodeIndex);
 
+   /**
+    * Aftert unmarshalling on the slave, this method will be called to init the stage with slave's state.
+    */
    public void initOnNode(NodeState nodeState);
 
+   /**
+    * Do whatever on the slave. This will only be called after {@link #executeOnNode()} is called.
+    * @return an response that will be serialized and send back to the server.
+    */
    DistStageAck executeOnNode();
 
+   /**
+    * After all slaves replied through {@link #executeOnNode()}, this method will be called on the server.
+    * @return returning false will cause the benchmark to stop.
+    */
    boolean processAckOnServer(List<DistStageAck> acks);
 
    public DistStage clone() throws CloneNotSupportedException;
