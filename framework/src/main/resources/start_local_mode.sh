d04a2f8a4a0eb3d43862d10c2e5b946f3a69dc53
Manik Surtani <manik@surtani.org>
2009-11-09 01:45:29 +0000

    Provide plugin details in script

--- framework/src/main/resources/start_local_mode.sh
+++ framework/src/main/resources/start_local_mode.sh
@@ -6,7 +6,11 @@ if [ "x$CBF_HOME" = "x" ]; then DIRNAME=`dirname $0`; CBF_HOME=`cd $DIRNAME/..; 
 
 help_and_exit() {
   wrappedecho "Usage: "
-  wrappedecho '  $ start_local_mode.sh -pc PLUGIN_CONFIG_FILE -c BENCHMARK_CONFIG_FILE'
+  wrappedecho '  $ start_local_mode.sh [-n BENCHMARK_NAME] -p PLUGIN -pc PLUGIN_CONFIG_FILE -c BENCHMARK_CONFIG_FILE'
+  wrappedecho ""
+  wrappedecho "   -n     String to name the benchmark.  Defaults to current date."
+  wrappedecho ""
+  wrappedecho "   -p     Plugin to benchmark.  Needs to exist in the plugins directory.  This is REQUIRED."
   wrappedecho ""
   wrappedecho "   -pc    Plugin configuration file, relative to its location in plugins/<PLUGIN>/conf. E.g., repl-sync.xml. This is REQUIRED."
   wrappedecho ""
@@ -18,6 +22,9 @@ help_and_exit() {
 }
 
 welcome "This script is used to launch a LOCAL mode test."
+
+BENCHNAME=`date +%Y%m%d_%H%M`
+BENCHNAME="benchmark-${BENCHNAME}"
 
 ### read in any command-line params
 while ! [ -z $1 ]
@@ -31,7 +38,15 @@ do
       CONF=$2
       shift
       ;;
-    *)
+    "-p")
+      PLUGIN=$2
+      shift
+      ;;
+    "-n")
+      BENCHNAME=$2
+      shift
+      ;;
+    *)    
       help_and_exit
       ;;
   esac
@@ -43,6 +58,12 @@ if [ -z $PLUGIN_CONF ] ; then
   help_and_exit
 fi
 
+if [ -z $PLUGIN ] ; then
+  echo "FATAL: required information (-p) missing!"
+  help_and_exit
+fi
+
+
 if [ -z $CONF ] ; then
   echo "FATAL: required information (-c) missing!"
   help_and_exit
@@ -51,4 +72,4 @@ fi
 add_fwk_to_classpath
 add_plugin_to_classpath $PLUGIN
 
-java -classpath $CP -Djava.net.preferIPv4Stack=true -Dcbf.plugin.configfile=${PLUGIN_CONF} org.cachebench.LocalModeRunner ${CONF}
+java -classpath $CP -Djava.net.preferIPv4Stack=true -Dcbf.plugin.configfile=${PLUGIN_CONF} -Dcbf.benchmarkname=${BENCHNAME} org.cachebench.LocalModeRunner -c ${CONF}
