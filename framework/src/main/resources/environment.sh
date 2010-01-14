bc43cd8a1d3e139da3ebc07345450cb62df4c1c0
Manik Surtani <manik@surtani.org>
2010-01-14 00:54:29 +0000

    fixed path

--- framework/src/main/resources/environment.sh
+++ framework/src/main/resources/environment.sh
@@ -5,6 +5,6 @@
 ### Set your bind address for the tests to use. Could be an IP, host name or a reference to an environment variable.
 BIND_ADDRESS=${MYTESTIP_2}
 JVM_OPTS="-Xms1G -Xmx1G"
-JPROFILER_HOME=${HOME}/jprofiler
+JPROFILER_HOME=${HOME}/jprofiler6
 JPROFILER_CFG_ID=103
 
