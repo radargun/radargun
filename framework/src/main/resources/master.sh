30e612db0bcb6d3f1903b8cf77f27ed806cc69f2
Paul Ferraro <paul.ferraro@redhat.com>
2010-01-27 00:22:50 -0500

    Replaced -p -n -s arguments with explicit slave node list Default master node to `hostname` Try system property for number of slaves in benchmark.xml

--- framework/src/main/resources/master.sh
+++ framework/src/main/resources/master.sh
@@ -5,12 +5,17 @@ if [ "x$CBF_HOME" = "x" ]; then DIRNAME=`dirname $0`; CBF_HOME=`cd $DIRNAME/..; 
 . ${CBF_HOME}/bin/includes.sh
 
 CONFIG=./conf/benchmark.xml
+SLAVE_COUNT_ARG=""
 
 help_and_exit() {
   echo "Usage: "
-  echo '  $ master.sh -[c]'
+  echo '  $ master.sh [-c CONFIG] [-s SLAVE_COUNT]'
   echo ""
-  echo "   -c        Path to the framework configuration XML file. Optional - if not supplied benchmark will load ./conf/benchmark.xml"
+  echo "   -c       Path to the framework configuration XML file. Optional - if not supplied benchmark will load ./conf/benchmark.xml"
+  echo ""
+  echo "   -s       Number of slaves.  Defaults to maxSize attribute in framework configuration XML file."
+  echo ""
+  echo "   -h       Displays this help screen"
   echo ""
 
   exit 0
@@ -22,11 +27,19 @@ welcome "This script is used to launch the master process, which coordinates tes
 while ! [ -z $1 ]
 do
   case "$1" in
+    "-s")
+      SLAVE_COUNT_ARG="-Dslaves=$2 "
+      shift
+      ;;
     "-c")
       CONFIG=$2
       shift
       ;;
+    "-h")
+      help_and_exit
+      ;;
     *)
+      echo "Warning: unknown argument ${1}" 
       help_and_exit
       ;;
   esac
@@ -36,7 +49,7 @@ done
 add_fwk_to_classpath
 set_env
 D_VARS="-Djava.net.preferIPv4Stack=true"
-java ${JVM_OPTS} -classpath $CP ${D_VARS} -Dbind.address=${BIND_ADDRESS} org.cachebench.LaunchMaster -config ${CONFIG} > stdout_master.out 2>&1 &
+java ${JVM_OPTS} -classpath $CP ${D_VARS} $SLAVE_COUNT_ARG -Dbind.address=${BIND_ADDRESS} org.cachebench.LaunchMaster -config ${CONFIG} > stdout_master.out 2>&1 &
 export CBF_MASTER_PID=$!
 HOST_NAME=`hostname`
 echo "Master's PID is $CBF_MASTER_PID running on ${HOST_NAME}"
