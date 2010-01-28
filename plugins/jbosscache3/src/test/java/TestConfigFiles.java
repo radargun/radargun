eab979b8f352098dcf41091977b971833d870977
Manik Surtani <manik@surtani.org>
2010-01-28 11:11:21 +0000

    Updated stuff

---
+++ plugins/jbosscache3/src/test/java/TestConfigFiles.java
@@ -0,0 +1,35 @@
+import org.jboss.cache.Cache;
+import org.jboss.cache.DefaultCacheFactory;
+import org.testng.annotations.Test;
+
+import static org.jboss.cache.CacheStatus.STARTED;
+import static org.jboss.cache.config.Configuration.CacheMode.LOCAL;
+import static org.jboss.cache.lock.IsolationLevel.READ_COMMITTED;
+import static org.jboss.cache.lock.IsolationLevel.REPEATABLE_READ;
+
+@Test (enabled = false)
+public class TestConfigFiles {
+   public void testConfigFilesRR() {
+      Cache cache = new DefaultCacheFactory().createCache("mvcc/mvcc-local-RR.xml");
+      cache.start();
+
+      assert cache.getCacheStatus() == STARTED;
+
+      assert cache.getConfiguration().getIsolationLevel() == REPEATABLE_READ;
+      assert cache.getConfiguration().getCacheMode() == LOCAL;
+
+      cache.stop();
+   }
+
+   public void testConfigFilesRC() {
+      Cache cache = new DefaultCacheFactory().createCache("mvcc/mvcc-local-RC.xml");
+      cache.start();
+
+      assert cache.getCacheStatus() == STARTED;
+
+      assert cache.getConfiguration().getIsolationLevel() == READ_COMMITTED;
+      assert cache.getConfiguration().getCacheMode() == LOCAL;
+
+      cache.stop();
+   }
+}
