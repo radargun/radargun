f5fd707f334cfcad5726a644bb748efd9d09c837
Mircea Markus <mircea.markus@gmail.com>
2010-02-17 12:01:21 +0200

    prepared 1.1.0 branch

--- framework/src/main/java/org/cachebench/Version.java
+++ framework/src/main/java/org/cachebench/Version.java
@@ -1,7 +1,7 @@
 package org.cachebench;
 
 public class Version {
-   private static final String ver = "1.0.0-GA";
+   private static final String ver = "1.1.0-SNAPSHOT";
    public static void main(String[] args) {
       System.out.println("=== CacheBenchFwk ===");
       System.out.println(" version: " + ver);
