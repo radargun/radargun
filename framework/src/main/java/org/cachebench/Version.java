aee177701e92d9c4a1132a7fe9ba775e9924ce3d
Mircea Markus <mircea.markus@gmail.com>
2010-02-17 11:04:00 +0200

    prepared 1.0.0.GA

--- framework/src/main/java/org/cachebench/Version.java
+++ framework/src/main/java/org/cachebench/Version.java
@@ -1,7 +1,7 @@
 package org.cachebench;
 
 public class Version {
-   private static final String ver = "1.0.0-SNAPSHOT";
+   private static final String ver = "1.0.0-GA";
    public static void main(String[] args) {
       System.out.println("=== CacheBenchFwk ===");
       System.out.println(" version: " + ver);
--- framework/src/main/resources/chart_gen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## Load includes
-if [ "x$CBF_HOME" = "x" ]; then DIRNAME=`dirname $0`; CBF_HOME=`cd $DIRNAME/..; pwd` ; fi; export CBF_HOME
-. ${CBF_HOME}/bin/includes.sh
-
-add_fwk_to_classpath
-set_env
-java ${JVM_OPTS} -classpath $CP org.cachebench.reporting.ChartGenerator ${*}
