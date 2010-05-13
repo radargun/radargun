fd7a9ef177f8b71e58c7b62c97de1a415498d36e
Manik Surtani <manik@surtani.org>
2010-05-13 16:00:44 +0100

    Cleanup script

---
+++ framework/src/main/resources/clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## Load includes
+if [ "x$CBF_HOME" = "x" ]; then DIRNAME=`dirname $0`; CBF_HOME=`cd $DIRNAME/..; pwd` ; fi; export CBF_HOME
+. ${CBF_HOME}/bin/includes.sh
+
+rm *.out > /dev/null
+rm *.log > /dev/null
+rm -rf reports > /dev/null
+
