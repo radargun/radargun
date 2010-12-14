dafa88d79b7fb72f75cf512a5f9a2dcccabcb84c
Mircea Markus <Mircea.Markus@gmail.com>
2010-12-14 15:29:53 +0000

    updated versioning

--- framework/src/main/java/org/radargun/Version.java
+++ framework/src/main/java/org/radargun/Version.java
@@ -1,7 +1,7 @@
 package org.radargun;
 
 public class Version {
-   private static final String ver = "1.1.0-SNAPSHOT";
+   private static final String ver = "1.0.0-FINAL";
    public static void main(String[] args) {
       System.out.println("=== RadarGun ===");
       System.out.println("");
