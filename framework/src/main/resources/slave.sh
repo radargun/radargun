91ab2c7530cf59b0f6026385ac07b8cdf0cb86bf
Dan Berindei <dan.berindei@gmail.com>
2011-03-19 22:15:19 +0200

    Use the LOG4J_PREFIX for stdout files, too.

--- framework/src/main/resources/slave.sh
+++ framework/src/main/resources/slave.sh
@@ -72,10 +72,9 @@ add_fwk_to_classpath
 set_env
 
 D_VARS="-Djava.net.preferIPv4Stack=true -Dlog4j.file.prefix=${LOG4J_PREFIX} -Dbind.address=${BIND_ADDRESS} -Djgroups.bind_addr=${BIND_ADDRESS}"
-HOST_NAME=`hostname`
-echo "${JAVA} ${JVM_OPTS} ${D_VARS} -classpath $CP org.radargun.Slave ${CONF}" > stdout_slave_${HOST_NAME}.out
-echo "--------------------------------------------------------------------------------" >> stdout_slave_${HOST_NAME}.out
-nohup ${JAVA} ${JVM_OPTS} ${D_VARS} -classpath $CP org.radargun.Slave ${CONF} >> stdout_slave_${HOST_NAME}.out 2>&1 &
+echo "${JAVA} ${JVM_OPTS} ${D_VARS} -classpath $CP org.radargun.Slave ${CONF}" > stdout_slave_${LOG4J_PREFIX}.out
+echo "--------------------------------------------------------------------------------" >> stdout_slave_${LOG4J_PREFIX}.out
+nohup ${JAVA} ${JVM_OPTS} ${D_VARS} -classpath $CP org.radargun.Slave ${CONF} >> stdout_slave_${LOG4J_PREFIX}.out 2>&1 &
 echo "... done! Slave process started on host ${HOST_NAME}!"
 echo ""
 
