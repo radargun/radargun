9af9fa5015d5a1a65ab86151713d95d17c653647
rvansa <rvansa@redhat.com>
2012-11-14 16:25:33 +0100

    Added async logging

--- framework/src/main/java/org/radargun/stages/StackTraceWatchdogStage.java
+++ framework/src/main/java/org/radargun/stages/StackTraceWatchdogStage.java
@@ -1,9 +1,10 @@
 package org.radargun.stages;
 
-import java.util.Arrays;
-import java.util.Map;
-
 import org.radargun.DistStageAck;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * 
@@ -17,9 +18,15 @@ public class StackTraceWatchdogStage extends AbstractDistStage {
    private String mask = null;
    private boolean onlyStuck = true;
    private int shortStack = 10;
-   
+   private boolean asyncLogging;
+
    private static final String WATCHDOG = "__watchdog__";
-   private static Thread watchDogThread;
+   private static final String WATCHDOG_LOGGING = "__watchdog_logging__";
+   private static WatchDogThread watchDogThread;
+   private static LoggingThread loggingThread;
+
+   private Queue<String> messageQueue = new LinkedList<String>();
+   private DateFormat formatter = new SimpleDateFormat("HH:mm:ss,SSS");
 
    public StackTraceWatchdogStage() {
       // nada
@@ -32,63 +39,111 @@ public class StackTraceWatchdogStage extends AbstractDistStage {
       }
       synchronized (StackTraceWatchdogStage.class) {
          if (watchDogThread != null) {
-            log.trace("Watchdog already running");
+            log.warn("Watchdog already running");
+         } else {
+            watchDogThread = new WatchDogThread();
+            watchDogThread.setName(WATCHDOG);
+            watchDogThread.setDaemon(true);
+            watchDogThread.start();
+            log.trace("Started watchdog");
          }
-         watchDogThread = new Thread(new Runnable() {
-   
-            private Map<Thread, StackTraceElement[]> lastStacks;
-            
-            @Override
-            public void run() {
-               while (true) {
-                  Map<Thread, StackTraceElement[]> stacks = Thread.getAllStackTraces();
-                  for (Thread t : stacks.keySet()) {
-                     String name = t.getName(); 
-                     if (!name.equals(WATCHDOG) && (mask == null || name.contains(mask))) {
-                        StackTraceElement[] stack = stacks.get(t);
-                        if (stack.length < shortStack) {
-                           continue;
-                        }
-                        boolean stuck = isStuck(t, stack);
-                        if (!onlyStuck || stuck) {
-                           traceStack(t, stack, stuck);
-                        }
-                     }
-                  }
-                  lastStacks = stacks;                
-                  try {
-                     Thread.sleep(period);
-                  } catch (InterruptedException e) {
-                  }
-               }
+         if (asyncLogging) {
+            if (loggingThread != null) {
+               log.warn("Logging thread already running");
+            } else {
+               loggingThread = new LoggingThread();
+               loggingThread.setName(WATCHDOG_LOGGING);
+               loggingThread.setDaemon(true);
+               loggingThread.start();
+               log.trace("Started logging thread");
             }
-   
-            private void traceStack(Thread t, StackTraceElement[] stack, boolean stuck) {
-               StringBuilder sb = new StringBuilder();
-               sb.append("Stack for thread ");
-               sb.append(t.getName());
-               if (stuck) sb.append("(possibly stuck)");
-               sb.append(":\n");
-               for (StackTraceElement ste : stack) {
-                  sb.append(ste.toString());
-                  sb.append('\n');
-               }
-               log.trace(sb.toString());
-            }
-   
-            private boolean isStuck(Thread t, StackTraceElement[] stack) {
-               if (lastStacks == null) return false;
-               StackTraceElement[] lastStack = lastStacks.get(t);
-               if (lastStack == null) return false;
-               return Arrays.deepEquals(lastStack, stack);
-            }
-            
-         }, WATCHDOG);
-         watchDogThread.setDaemon(true);
-         watchDogThread.start();
-         log.trace("Started watchdog");
+         }
       }
       return newDefaultStageAck();
+   }
+
+   private class WatchDogThread extends Thread {
+      private Map<Thread, StackTraceElement[]> lastStacks;
+
+      @Override
+      public void run() {
+         while (true) {
+            log("Running check");
+            long pre = System.nanoTime();
+            Map<Thread, StackTraceElement[]> stacks = Thread.getAllStackTraces();
+            long post = System.nanoTime();
+            log("Thread.getAllStackTraces() took " + (post - pre) + " nanoseconds");
+            for (Thread t : stacks.keySet()) {
+               String name = t.getName();
+               if (!name.equals(WATCHDOG) && (mask == null || name.contains(mask))) {
+                  StackTraceElement[] stack = stacks.get(t);
+                  if (stack.length < shortStack) {
+                     continue;
+                  }
+                  boolean stuck = isStuck(t, stack);
+                  if (!onlyStuck || stuck) {
+                     traceStack(t, stack, stuck);
+                  }
+               }
+            }
+            lastStacks = stacks;
+            try {
+               Thread.sleep(period);
+            } catch (InterruptedException e) {
+            }
+         }
+      }
+
+      private boolean isStuck(Thread t, StackTraceElement[] stack) {
+         if (lastStacks == null) return false;
+         StackTraceElement[] lastStack = lastStacks.get(t);
+         if (lastStack == null) return false;
+         return Arrays.deepEquals(lastStack, stack);
+      }
+   }
+
+   private class LoggingThread extends Thread {
+      @Override
+      public void run() {
+         while (true) {
+            String message;
+            synchronized (messageQueue) {
+               if (messageQueue.isEmpty()) {
+                  try {
+                     messageQueue.wait();
+                  } catch (InterruptedException e) {
+                  }
+                  continue;
+               }
+               message = messageQueue.poll();
+            }
+            log.trace(message);
+         }
+      }
+   }
+
+   private void traceStack(Thread t, StackTraceElement[] stack, boolean stuck) {
+      StringBuilder sb = new StringBuilder();
+      sb.append("Stack for thread ");
+      sb.append(t.getName());
+      if (stuck) sb.append("(possibly stuck)");
+      sb.append(":\n");
+      for (StackTraceElement ste : stack) {
+         sb.append(ste.toString());
+         sb.append('\n');
+      }
+      log(sb.toString());
+   }
+
+   private void log(String message) {
+      if (asyncLogging) {
+         synchronized (messageQueue) {
+            messageQueue.add(formatter.format(new Date()) + ": " + message);
+            messageQueue.notify();
+         }
+      } else {
+         log.trace(message);
+      }
    }
 
    @Override
@@ -131,5 +186,9 @@ public class StackTraceWatchdogStage extends AbstractDistStage {
     */
    public void setShortStack(int length) {
       this.shortStack = length;
+   }
+
+   public void setAsyncLogging(boolean asyncLogging) {
+      this.asyncLogging = asyncLogging;
    }
 }
