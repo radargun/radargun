f8c1da60e9ae65cd56f0080bf314e866b1e90c89
rvansa <rvansa@redhat.com>
2012-12-05 11:03:48 +0100

    Attributes may contain simple post-fix expressions in form #{expression}

--- framework/src/main/java/org/radargun/config/ConfigHelper.java
+++ framework/src/main/java/org/radargun/config/ConfigHelper.java
@@ -9,6 +9,7 @@ import java.beans.PropertyEditorManager;
 import java.lang.reflect.Method;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Stack;
 
 /**
  * Contains various configuration helper methods needed by different parsers.
@@ -104,9 +105,11 @@ public class ConfigHelper {
       if (val.length() <= "${}".length())
          return val;
       String originalVal = val;
-      if (val.indexOf("${") == 0) {
+      if (val.startsWith("${")) {
+         int end = val.indexOf('}');
+         if (end < 0) throw new IllegalArgumentException(val);
          //get rid of '${' and '}'
-         val = val.substring(2, val.length() - 1);
+         val = val.substring(2, end);
          int separator = val.indexOf(':');
          if (separator > 0) {
             String defaultValue = val.substring(0, separator);
@@ -128,8 +131,115 @@ public class ConfigHelper {
                return sysProp;
             }
          }
+      } else if (val.startsWith("#{") && val.endsWith("}")) {
+         Stack<String> stack = new Stack<String>();
+         return eval(stack, val.substring(2, val.length() - 1).trim());
       } else {
          return val;
+      }
+   }
+
+   private static String eval(Stack<String> stack, String expression) {
+      while (true) {
+         int next;
+         if (expression.startsWith("+")) {
+            stack.push(plus(stack.pop(), stack.pop()));
+            next = 1;
+         } else if (expression.startsWith("-")) {
+            stack.push(minus(stack.pop(), stack.pop()));
+            next = 1;
+         } else if (expression.startsWith("*")) {
+            stack.push(times(stack.pop(), stack.pop()));
+            next = 1;
+         } else if (expression.startsWith("..")) {
+            stack.push(range(stack.pop(), stack.pop()));
+            next = 2;
+         } else if (expression.startsWith(",")) {
+            stack.push(concat(stack.pop(), stack.pop()));
+            next = 1;
+         } else {
+            next = 0;
+            int inBraces = 0;
+            while (next < expression.length()) {
+               char c = expression.charAt(next);
+               if (Character.isWhitespace(c) && inBraces == 0) break;
+               else if (c == '{') inBraces++;
+               else if (c == '}') {
+                  if (inBraces <= 0) throw new IllegalArgumentException("Invalid braces nesting");
+                  inBraces--;
+               }
+               next++;
+            }
+            stack.push(checkForProps(expression.substring(0, next)));
+         }
+         while (next < expression.length() && Character.isWhitespace(expression.charAt(next))) ++next;
+         if (next == expression.length()) break;
+         expression = expression.substring(next);
+      }
+      if (stack.size() != 1) {
+         StringBuilder sb = new StringBuilder("Stack is" + (stack.empty() ? " empty" : ":"));
+         while (!stack.empty()) {
+            sb.append("\n\t").append(stack.pop());
+         }
+         throw new IllegalStateException(sb.toString());
+      }
+      return stack.pop();
+   }
+
+   private static String concat(String second, String first) {
+      return first + "," + second;
+   }
+
+   private static String range(String second, String first) {
+      try {
+         int from = Integer.parseInt(first);
+         int to = Integer.parseInt(second);
+         if (from > to) {
+            int temp = from;
+            from = to;
+            to = temp;
+         } else if (from == to) return String.valueOf(from);
+         StringBuilder sb = new StringBuilder(String.valueOf(from));
+         for (int i = from + 1; i <= to; ++i) sb.append(',').append(i);
+         return sb.toString();
+      }  catch (NumberFormatException e2) {
+         throw new IllegalArgumentException(first + " .. " + second);
+      }
+   }
+
+   private static String times(String second, String first) {
+      try {
+         return String.valueOf(Integer.parseInt(first) * Integer.parseInt(second));
+      } catch (NumberFormatException e) {
+         try {
+            return String.valueOf(Double.parseDouble(first) * Double.parseDouble(second));
+         } catch (NumberFormatException e2) {
+            throw new IllegalArgumentException(first + " * " + second);
+         }
+      }
+   }
+
+   private static String minus(String second, String first) {
+      try {
+         return String.valueOf(Integer.parseInt(first) - Integer.parseInt(second));
+      } catch (NumberFormatException e) {
+         try {
+            return String.valueOf(Double.parseDouble(first) - Double.parseDouble(second));
+         } catch (NumberFormatException e2) {
+            throw new IllegalArgumentException(first + " - " + second);
+         }
+      }
+   }
+
+   private static String plus(String second, String first) {
+      try {
+         return String.valueOf(Integer.parseInt(first) + Integer.parseInt(second));
+      } catch (NumberFormatException e) {
+         try {
+            return String.valueOf(Double.parseDouble(first) + Double.parseDouble(second));
+         } catch (NumberFormatException e2) {
+            throw new IllegalArgumentException(first + " + " + second);
+         }
       }
    }
 
