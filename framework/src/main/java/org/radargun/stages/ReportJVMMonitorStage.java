5309f82c0c906aba865f7c3eaba0696f2e99f7e3
rvansa <rvansa@redhat.com>
2013-02-19 13:10:47 +0100

    Locally generated JVM statistics

--- framework/src/main/java/org/radargun/stages/ReportJVMMonitorStage.java
+++ framework/src/main/java/org/radargun/stages/ReportJVMMonitorStage.java
@@ -18,37 +18,129 @@
  */
 package org.radargun.stages;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.radargun.config.Stage;
-import org.radargun.local.ReportDesc;
-import org.radargun.reporting.LocalSystemMonitorChart;
+import org.radargun.reporting.ClusterReport;
+import org.radargun.reporting.LineReportGenerator;
+import org.radargun.sysmonitor.AbstractActivityMonitor;
+import org.radargun.sysmonitor.CpuUsageMonitor;
+import org.radargun.sysmonitor.GcMonitor;
 import org.radargun.sysmonitor.LocalJmxMonitor;
+import org.radargun.sysmonitor.MemoryUsageMonitor;
+import org.radargun.utils.Utils;
 
 /**
- * 
+ *
  * Generate charts for JVM statistics on each slave node.
- * 
+ *
  * @author Alan Field &lt;afield@redhat.com&gt;
  */
 @Stage(doc = "Generate charts for JVM statistics on each slave node.")
 public class ReportJVMMonitorStage extends AbstractMasterStage {
 
+   Map<String, LocalJmxMonitor> sysMonitors;
+   String reportPrefix;
+
+   @SuppressWarnings("unchecked")
    @Override
    public boolean execute() throws Exception {
-      @SuppressWarnings("unchecked")
-      Map<String, LocalJmxMonitor> sysMonitors = (Map<String, LocalJmxMonitor>) masterState.get(StartJVMMonitorStage.monitorKey);
+      this.sysMonitors = (Map<String, LocalJmxMonitor>) masterState.get(StartJVMMonitorStage.monitorKey);
       if (!sysMonitors.isEmpty()) {
-         ReportDesc reportDesc = new ReportDesc();
-         LocalJmxMonitor monitor = sysMonitors.values().iterator().next();
-         reportDesc.addReportItem(monitor.getProductName(), monitor.getConfigName());
-         reportDesc.setReportName(monitor.getProductName() + " (" + monitor.getConfigName() + ") on "
-               + sysMonitors.size() + " node(s)");
-         new LocalSystemMonitorChart(sysMonitors).generate(reportDesc);
          masterState.remove(StartJVMMonitorStage.monitorKey);
+         LocalJmxMonitor monitor = sysMonitors.values().iterator().next();
+         reportPrefix = monitor.getProductName() + " (" + monitor.getConfigName() + ") on " + sysMonitors.size()
+               + " node(s)";
+         this.generateCpu();
+         this.generateGc();
+         this.generateMemory();
          return true;
       }
       this.log.error("Could not retrieve the JVM monitors");
       return false;
+   }
+
+   private void generateMemory() {
+      ClusterReport lcr = new ClusterReport();
+      lcr.init("Time(sec)", "Memory(Mb)", "Memory consumption", "");
+      for (String s : sysMonitors.keySet()) {
+         MemoryUsageMonitor memMonitor = sysMonitors.get(s).getMemoryMonitor();
+         memMonitor.convertToMb();
+         populateGraph(lcr, "mem-" + s, memMonitor);
+      }
+      generateReport(lcr, "memory_usage");
+   }
+
+   private void generateCpu() {
+      ClusterReport lcr = new ClusterReport();
+      lcr.init("Time(sec)", "CPU", "CPU Usage (%)", "");
+      for (String s : sysMonitors.keySet()) {
+         CpuUsageMonitor cpuMonitor = sysMonitors.get(s).getCpuMonitor();
+         populateGraph(lcr, "cpu-" + s, cpuMonitor);
+      }
+      generateReport(lcr, "cpu_usage");
+   }
+
+   private void generateGc() {
+      ClusterReport lcr = new ClusterReport();
+      lcr.init("Time(sec)", "GC", "GC Usage (%)", "");
+      for (String s : sysMonitors.keySet()) {
+         GcMonitor gcMonitor = sysMonitors.get(s).getGcMonitor();
+         populateGraph(lcr, "gc-" + s, gcMonitor);
+      }
+      generateReport(lcr, "gc_usage");
+   }
+
+   private void populateGraph(ClusterReport lcr, String s, AbstractActivityMonitor activityMonitor) {
+      StringBuilder reportCsvContent = new StringBuilder();
+      reportCsvContent.append("NODE,TIME,MEASUREMENT\n");
+      int measuringFrequencySecs = (int) TimeUnit.MILLISECONDS.toSeconds(LocalJmxMonitor.MEASURING_FREQUENCY);
+      LinkedHashMap<Integer, BigDecimal> graphData = activityMonitor.formatForGraph(measuringFrequencySecs, 25);
+      for (Map.Entry<Integer, BigDecimal> e : graphData.entrySet()) {
+         lcr.addCategory(s, e.getKey(), e.getValue());
+         reportCsvContent.append('\n').append(s + "," + e.getKey() + "," + e.getValue());
+      }
+
+      try {
+         createOutputFile(reportPrefix + "-" + s + ".csv", reportCsvContent);
+      } catch (IOException e1) {
+         log.error("Failed to write CSV file", e1);
+      }
+   }
+
+   private void generateReport(ClusterReport lcr, String fileNameNoExtension) {
+      try {
+
+         LineReportGenerator.generate(lcr, GenerateChartStage.REPORTS, reportPrefix + "-" + fileNameNoExtension);
+      } catch (Exception e) {
+         log.error(e.getMessage(), e);
+      }
+   }
+
+   private void createOutputFile(String fileName, StringBuilder reportCsvContent) throws IOException {
+      File parentDir = new File(GenerateChartStage.REPORTS);
+      if (!parentDir.exists()) {
+         if (!parentDir.mkdirs())
+            throw new RuntimeException(parentDir.getAbsolutePath() + " does not exist and could not be created!");
+      }
+
+      File reportFile = Utils.createOrReplaceFile(parentDir, fileName);
+      if (!reportFile.exists()) {
+         throw new IllegalStateException(reportFile.getAbsolutePath() + " was deleted? Not allowed to delete report file during test run!");
+      }
+      PrintWriter writer = null;
+      try {
+         writer = new PrintWriter(reportFile);
+         writer.append(reportCsvContent.toString());
+      } finally {
+         if (writer != null) writer.close();
+      }
+
    }
 }
