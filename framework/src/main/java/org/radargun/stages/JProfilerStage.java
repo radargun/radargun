52d8088160aa4eae1a159a530626df27fb50a441
Alan Field <afield@redhat.com>
2013-04-19 17:38:53 -0400

    Add support for JProfiler 7 and scaling benchmarks in JProfilerStage
    Modify the SAVE_SNAPSHOT command to use a directory, and then generate
    the file name based on the product name, configuration name, and node
    index

--- framework/src/main/java/org/radargun/stages/JProfilerStage.java
+++ framework/src/main/java/org/radargun/stages/JProfilerStage.java
@@ -1,35 +1,42 @@
 package org.radargun.stages;
 
+import java.lang.management.ManagementFactory;
+import java.util.Collection;
+
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+
 import org.radargun.DistStageAck;
 import org.radargun.config.Property;
 import org.radargun.config.Stage;
-
-import javax.management.*;
-import java.lang.management.ManagementFactory;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.StringTokenizer;
+import org.radargun.state.MasterState;
 
 /**
- * Stage for invoking operations on JProfiler.
- * Remember to set up JVM args: "-agentpath:/path/to/libjprofilerti.so=offline,id=100,config=/path/to/configuration.xml"
- *
+ * Stage for invoking operations on JProfiler. Remember to set up JVM args:
+ * "-agentpath:/path/to/libjprofilerti.so=offline,id=100,config=/path/to/configuration.xml"
+ * 
  * @author rvansa
  * @since 4.0
  */
-@Stage(doc = "Stage for invoking operations on JProfiler.\nRemember to set up JVM args: " +
-      "\"-agentpath:/path/to/libjprofilerti.so=offline,id=100,config=/path/to/configuration.xml\"")
+@Stage(doc = "Stage for invoking operations on JProfiler.\nRemember to set up JVM args: "
+      + "\"-agentpath:/path/to/libjprofilerti.so=offline,id=100,config=/path/to/configuration.xml\"")
 public class JProfilerStage extends AbstractDistStage {
 
-   public static final String CONTROLLER_OBJECT_NAME = "com.jprofiler.api.agent.mbean:type=Controller";
+   public static final String JPROFILER6_CONTROLLER_OBJECT_NAME = "com.jprofiler.api.agent.mbean:type=Controller";
+   public static final String JPROFILER7_CONTROLLER_OBJECT_NAME = "com.jprofiler.api.agent.mbean:type=RemoteController";
+   private ObjectName objectName;
+   private String configName;
 
    public enum Operation {
-      NO_OPERATION(null, null),
-      START_METHOD_STATS_RECORDING("startMethodStatsRecording", new String[] {}),
-      STOP_METHOD_STATS_RECORDING("startMethodStatsRecording", new String[] {}),
-      START_CPU_RECORDING("startCPURecording", new String[] { boolean.class.getName() }),
-      STOP_CPU_RECORDING("stopCPURecording", new String[] {}),
-      SAVE_SNAPSHOT("saveSnapshot", new String[] { String.class.getName() });
+      NO_OPERATION(null, null), START_METHOD_STATS_RECORDING("startMethodStatsRecording", new String[] {}), STOP_METHOD_STATS_RECORDING(
+            "stopMethodStatsRecording", new String[] {}), START_CPU_RECORDING("startCPURecording",
+            new String[] { boolean.class.getName() }), STOP_CPU_RECORDING("stopCPURecording", new String[] {}), SAVE_SNAPSHOT(
+            "saveSnapshot", new String[] { String.class.getName() }), START_MEMORY_RECORDING("startAllocRecording",
+            new String[] { boolean.class.getName() }), STOP_MEMORY_RECORDING("stopAllocRecording", new String[] {}), START_MONITOR_RECORDING(
+            "startMonitorRecording", new String[] {}), STOP_MONITOR_RECORDING("stopMonitorRecording", new String[] {}), START_THREAD_RECORDING(
+            "startThreadProfiling", new String[] {}), STOP_THREAD_RECORDING("stopThreadProfiling", new String[] {}), START_VM_TELEMETRY_RECORDING(
+            "startVMTelemetryRecording", new String[] {}), STOP_VM_TELEMETRY_RECORDING("stopVMTelemetryRecording",
+            new String[] {});
 
       private String name;
       private String[] signature;
@@ -51,12 +58,16 @@ public class JProfilerStage extends AbstractDistStage {
    @Property(optional = false, doc = "Operations that should be invoked on the Controller")
    protected Collection<Operation> operations;
 
-   @Property(doc = "Path where the snapshot should be written (for SAVE_SNAPSHOT).")
-   protected String snapshotOutput;
+   @Property(doc = "Directory where the snapshot should be written (for SAVE_SNAPSHOT).")
+   protected String snapshotDirectory;
 
-   @Property(doc = "If true, any previously accumulated CPU profiling data will be discarded. If false, CPU data will" +
-         "be accumulated across pairs of invocations of START_CPU_RECORDING and STOP_CPU_RECORDING. Default is false.")
+   @Property(doc = "If true, any previously accumulated CPU profiling data will be discarded. If false, CPU data will"
+         + "be accumulated across pairs of invocations of START_CPU_RECORDING and STOP_CPU_RECORDING. Default is false.")
    protected boolean resetCPUStats;
+
+   @Property(doc = "If true, any previously accumulated Memory profiling data will be discarded. If false, CPU data will"
+         + "be accumulated across pairs of invocations of START_MEMORY_RECORDING and STOP_MEMORY_RECORDING. Default is false.")
+   protected boolean resetMemoryStats;
 
    @Override
    public DistStageAck executeOnSlave() {
@@ -67,36 +78,47 @@ public class JProfilerStage extends AbstractDistStage {
       for (Operation operation : operations) {
          MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
          try {
-            ObjectName objectName = new ObjectName(CONTROLLER_OBJECT_NAME);
+            objectName = new ObjectName(JPROFILER7_CONTROLLER_OBJECT_NAME);
+            if (!mbs.isRegistered(objectName)) {
+               objectName = new ObjectName(JPROFILER6_CONTROLLER_OBJECT_NAME);
+               if (!mbs.isRegistered(objectName)) {
+                  log.info("JProfiler not enabled on this node.");
+                  return newDefaultStageAck();
+               }
+            }
             mbs.invoke(objectName, operation.getName(), getParams(operation), operation.getSignature());
-         } catch (InstanceNotFoundException e) {
-            return error(e);
-         } catch (MalformedObjectNameException e) {
-            return error(e);
-         } catch (ReflectionException e) {
-            return error(e);
-         } catch (MBeanException e) {
+         } catch (Exception e) {
             return error(e);
          }
       }
       return newDefaultStageAck();
    }
 
+   @Override
+   public void initOnMaster(MasterState masterState, int slaveIndex) {
+      configName = masterState.configNameOfTheCurrentBenchmark();
+   }
+
    private Object[] getParams(Operation operation) {
       switch (operation) {
-         case NO_OPERATION:
-            return null;
-         case STOP_METHOD_STATS_RECORDING:
-         case START_METHOD_STATS_RECORDING:
-         case STOP_CPU_RECORDING:
-            return new Object[0];
-         case SAVE_SNAPSHOT:
-            if (snapshotOutput == null) throw new NullPointerException("snapshotOutput not set");
-            return new Object[] { snapshotOutput };
-         case START_CPU_RECORDING:
-            return new Object[] { resetCPUStats };
-         default:
-            throw new UnsupportedOperationException();
+      case NO_OPERATION:
+         return null;
+      case STOP_METHOD_STATS_RECORDING:
+      case START_METHOD_STATS_RECORDING:
+      case STOP_CPU_RECORDING:
+      case STOP_MEMORY_RECORDING:
+         return new Object[0];
+      case SAVE_SNAPSHOT:
+         if (snapshotDirectory == null)
+            throw new NullPointerException("snapshotOutput not set");
+         return new Object[] { snapshotDirectory + this.productName + "-" + this.configName + "-node" + this.slaveIndex
+               + "-profiler.jps" };
+      case START_CPU_RECORDING:
+         return new Object[] { resetCPUStats };
+      case START_MEMORY_RECORDING:
+         return new Object[] { resetMemoryStats };
+      default:
+         throw new UnsupportedOperationException();
       }
    }
 
