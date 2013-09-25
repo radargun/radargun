c97381d1c6fcb5cdf743ac795f0da7f07f1e976f
rvansa <rvansa@redhat.com>
2013-09-25 10:16:08 +0200

    Fix for cross-site tests: Using key generator

--- framework/src/main/java/org/radargun/stages/XSReplLoadStage.java
+++ framework/src/main/java/org/radargun/stages/XSReplLoadStage.java
@@ -5,7 +5,8 @@ import org.radargun.config.Property;
 import org.radargun.config.Stage;
 import org.radargun.features.XSReplicating;
 import org.radargun.stages.helpers.Range;
-import org.radargun.state.MasterState;
+import org.radargun.stressors.KeyGenerator;
+import org.radargun.stressors.StringKeyGenerator;
 
 /**
  * Loads data into the cache with input cache name encoded into the value.
@@ -24,14 +25,9 @@ public class XSReplLoadStage extends AbstractDistStage {
    @Property(doc = "String encoded into the value so that the entry may be distinguished from entries loaded in " +
          "different load stages. Default is empty string.")
    private String valuePostFix = "";
-   
+
    public XSReplLoadStage() {
       // nada
-   }
-
-   @Override
-   public void initOnMaster(MasterState masterState, int slaveIndex) {
-      super.initOnMaster(masterState, slaveIndex);
    }
 
    public DistStageAck executeOnSlave() {
@@ -43,15 +39,19 @@ public class XSReplLoadStage extends AbstractDistStage {
          ack.setErrorMessage(error);
          return ack;
       }
+      KeyGenerator keyGenerator = (KeyGenerator) slaveState.get(KeyGenerator.KEY_GENERATOR);
+      if (keyGenerator == null) {
+         keyGenerator = new StringKeyGenerator();
+      }
       XSReplicating wrapper = (XSReplicating) slaveState.getCacheWrapper();
       String cacheName = wrapper.getMainCache();
       Range myRange = Range.divideRange(numEntries, wrapper.getSlaves().size(), wrapper.getSlaves().indexOf(getSlaveIndex()));
       for (int i = myRange.getStart(); i < myRange.getEnd(); ++i) {
          try {
             if (!delete) {
-               wrapper.put(cacheName, "key" + i, "value" + i + valuePostFix + "@" + cacheName);
+               wrapper.put(cacheName, keyGenerator.generateKey(i), "value" + i + valuePostFix + "@" + cacheName);
             } else {
-               wrapper.remove(cacheName, "key" + i);
+               wrapper.remove(cacheName, keyGenerator.generateKey(i));
             }
          } catch (Exception e) {
             log.error("Error inserting key " + i + " into " + cacheName);
