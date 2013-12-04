e47193431fb43d94421e5d30cf1bed8bd8ccd6c7
rvansa <rvansa@redhat.com>
2013-12-04 11:19:57 +0100

    Removed the JAXB configuration classes (not used || updated anymore)

--- framework/src/main/java/org/radargun/config/ConfigParser.java
+++ framework/src/main/java/org/radargun/config/ConfigParser.java
@@ -10,9 +10,6 @@ public abstract class ConfigParser {
    public abstract MasterConfig parseConfig(String config) throws Exception;
 
    public static ConfigParser getConfigParser() {
-      if (System.getProperties().contains("radargun.oldLauncher")) {
-         return new JaxbConfigParser();
-      }
       return new DomConfigParser();
    }
 }
