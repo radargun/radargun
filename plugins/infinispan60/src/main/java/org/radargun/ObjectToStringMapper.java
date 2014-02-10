c2ff4d5ff2bfea1c53cf3bde7d81e86ff2e7e27e
rvansa <rvansa@redhat.com>
2014-02-10 11:25:13 +0100

    Mapper for testing JDBC String based store

---
+++ plugins/infinispan60/src/main/java/org/radargun/ObjectToStringMapper.java
@@ -0,0 +1,34 @@
+package org.radargun;
+
+import org.infinispan.persistence.keymappers.TwoWayKey2StringMapper;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * Hackish generic implementation - keeps all objects in a map for reverse mapping.
+ * Requires that the key was stored before it is read from cachestore - no preloading!
+ *
+ * @author Radim Vansa &lt;rvansa@redhat.com&gt;
+ */
+public class ObjectToStringMapper implements TwoWayKey2StringMapper {
+
+   private ConcurrentMap<String, Object> map = new ConcurrentHashMap<String, Object>();
+
+   @Override
+   public boolean isSupportedType(Class<?> keyType) {
+      return true;
+   }
+
+   @Override
+   public String getStringMapping(Object key) {
+      String string = key.toString();
+      map.putIfAbsent(string, key);
+      return string;
+   }
+
+   @Override
+   public Object getKeyMapping(String stringKey) {
+      return map.get(stringKey);
+   }
+}
