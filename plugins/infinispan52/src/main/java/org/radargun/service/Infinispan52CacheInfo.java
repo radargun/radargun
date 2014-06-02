dd75c59ff30700ef5c07927422f8c3e1f2ceb6bc
Alan Field <afield@redhat.com>
2014-06-02 15:25:59 +0200

    Add a distributed callable to get total cache size
    Deleted Infinispan70CacheInfo class
    Remove reference from Infinispan70EmbeddedService and
    JDG63EmbeddedService classes
    Added getTotalSize() implementation to Infinispan52CacheInfo class
    Add CacheSizer distributed callable class

--- plugins/infinispan52/src/main/java/org/radargun/service/Infinispan52CacheInfo.java
+++ plugins/infinispan52/src/main/java/org/radargun/service/Infinispan52CacheInfo.java
@@ -1,12 +1,23 @@
 package org.radargun.service;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 import org.infinispan.AdvancedCache;
+import org.infinispan.distexec.DefaultExecutorService;
+import org.infinispan.distexec.DistributedExecutorService;
+import org.infinispan.distexec.DistributedTaskBuilder;
+import org.radargun.logging.Log;
+import org.radargun.logging.LogFactory;
 import org.radargun.traits.CacheInformation;
 
 /**
  * @author Radim Vansa &lt;rvansa@redhat.com&gt;
  */
 public class Infinispan52CacheInfo extends InfinispanCacheInfo {
+   private final Log log = LogFactory.getLog(this.getClass());
+
    public Infinispan52CacheInfo(InfinispanEmbeddedService service) {
       super(service);
    }
@@ -24,6 +35,26 @@ public class Infinispan52CacheInfo extends InfinispanCacheInfo {
       @Override
       public int getEntryOverhead() {
          return 152;
+      }
+      
+      @Override
+      public int getTotalSize() {
+         int totalSize = 0;
+         DistributedExecutorService des = new DefaultExecutorService(cache);
+         DistributedTaskBuilder<Integer> taskBuilder = des.createDistributedTaskBuilder(new CacheSizer());
+         List<Future<Integer>> futureList = des.submitEverywhere(taskBuilder.build());
+         for (Future<Integer> future : futureList) {
+            try {
+               totalSize += future.get().intValue();
+            } catch (InterruptedException e) {
+               log.error("The distributed task was interrupted.", e);
+               return -1;
+            } catch (ExecutionException e) {
+               log.error("An error occurred executing the distributed task.", e);
+               return -1;
+            }
+         }
+         return totalSize;         
       }
    }
 }
