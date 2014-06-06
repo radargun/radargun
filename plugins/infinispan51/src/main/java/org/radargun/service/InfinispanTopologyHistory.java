628952b7850ae8db22a232f4dd0e39724e90b3be
rvansa <rvansa@redhat.com>
2014-06-06 10:57:50 +0200

    Topology history for Infinispan server

--- plugins/infinispan51/src/main/java/org/radargun/service/InfinispanTopologyHistory.java
+++ plugins/infinispan51/src/main/java/org/radargun/service/InfinispanTopologyHistory.java
@@ -1,29 +1,16 @@
 package org.radargun.service;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 import org.infinispan.Cache;
 import org.infinispan.notifications.Listener;
 import org.infinispan.notifications.cachelistener.annotation.DataRehashed;
 import org.infinispan.notifications.cachelistener.annotation.TopologyChanged;
 import org.infinispan.notifications.cachelistener.event.DataRehashedEvent;
 import org.infinispan.notifications.cachelistener.event.TopologyChangedEvent;
-import org.radargun.logging.Log;
-import org.radargun.logging.LogFactory;
-import org.radargun.traits.TopologyHistory;
 
 /**
  * @author Radim Vansa &lt;rvansa@redhat.com&gt;
  */
-public class InfinispanTopologyHistory implements TopologyHistory {
-
-   protected final Log log = LogFactory.getLog(InfinispanBulkOperations.class);
-
-   private List<Event> topologyChanges = new ArrayList<Event>();
-   private List<Event> hashChanges = new ArrayList<Event>();
-
+public class InfinispanTopologyHistory extends AbstractTopologyHistory {
    protected final Infinispan51EmbeddedService service;
 
    public InfinispanTopologyHistory(Infinispan51EmbeddedService service) {
@@ -32,22 +19,6 @@ public class InfinispanTopologyHistory implements TopologyHistory {
 
    void registerListener(Cache<?, ?> cache) {
       cache.addListener(new TopologyAwareListener());
-   }
-
-   @Override
-   public List<TopologyHistory.Event> getTopologyChangeHistory() {
-      return deepCopy(topologyChanges);
-   }
-
-   @Override
-   public List<TopologyHistory.Event> getRehashHistory() {
-      return deepCopy(hashChanges);
-   }
-
-   private synchronized List<TopologyHistory.Event> deepCopy(List<Event> events) {
-      ArrayList<TopologyHistory.Event> newList = new ArrayList<TopologyHistory.Event>(events.size());
-      for (Event e : events) newList.add(e.copy());
-      return newList;
    }
 
    @Listener
@@ -66,74 +37,6 @@ public class InfinispanTopologyHistory implements TopologyHistory {
          int atStart = e.getMembersAtStart().size();
          int atEnd = e.getMembersAtEnd().size();
          addEvent(hashChanges, e.isPre(), atStart, atEnd);
-      }
-   }
-
-   private synchronized void addEvent(List<Event> list, boolean isPre, int atStart, int atEnd) {
-      if (isPre) {
-         list.add(new Event(false, atStart, atEnd));
-      } else {
-         int size = list.size();
-         if (size == 0 || list.get(size - 1).getEnded() != null) {
-            Event ev = new Event(true, atStart, atEnd);
-            list.add(ev);
-         } else {
-            list.get(size - 1).setEnded();
-         }
-      }
-   }
-
-   private static class Event extends TopologyHistory.Event {
-      private final Date started;
-      private Date ended;
-      private final int atStart;
-      private final int atEnd;
-
-      private Event(Date started, Date ended, int atStart, int atEnd) {
-         this.started = started;
-         this.ended = ended;
-         this.atStart = atStart;
-         this.atEnd = atEnd;
-      }
-
-      public Event(boolean finished, int atStart, int atEnd) {
-         if (finished) {
-            this.started = this.ended = new Date();
-         } else {
-            this.started = new Date();
-         }
-         this.atStart = atStart;
-         this.atEnd = atEnd;
-      }
-
-      @Override
-      public Date getStarted() {
-         return started;
-      }
-
-      public void setEnded() {
-         if (ended != null) throw new IllegalStateException();
-         ended = new Date();
-      }
-
-      @Override
-      public Date getEnded() {
-         return ended;
-      }
-
-      @Override
-      public int getMembersAtStart() {
-         return atStart;
-      }
-
-      @Override
-      public int getMembersAtEnd() {
-         return atEnd;
-      }
-
-      @Override
-      public TopologyHistory.Event copy() {
-         return new InfinispanTopologyHistory.Event(started, ended, atStart, atEnd);
       }
    }
 }
---
+++ plugins/infinispan60/src/main/java/org/radargun/service/Infinispan60ServerTopologyHistory.java
@@ -0,0 +1,47 @@
+package org.radargun.service;
+
+import java.util.regex.Pattern;
+
+/**
+ */
+public class Infinispan60ServerTopologyHistory extends AbstractTopologyHistory {
+   protected final InfinispanServerService service;
+
+   /**
+    * Requires logging:
+    * TRACE on org.infinispan.statetransfer.StateConsumerImpl,
+    * TRACE on org.infinispan.statetransfer.StateTransferManagerImpl
+    * DEBUG on org.infinispan.transaction.impl.TransactionTable
+    */
+   public Infinispan60ServerTopologyHistory(InfinispanServerService service) {
+      this.service = service;
+      service.registerAction(Pattern.compile(".*Installing new cache topology.*"), new Runnable() {
+         @Override
+         public void run() {
+            log.debug("Topology change started");
+            addEvent(topologyChanges, true, 0, 0);
+         }
+      });
+      service.registerAction(Pattern.compile(".*Topology changed, recalculating minTopologyId.*"), new Runnable() {
+         @Override
+         public void run() {
+            log.debug("Topology change finished");
+            addEvent(topologyChanges, false, 0, 0);
+         }
+      });
+      service.registerAction(Pattern.compile(".*Lock State Transfer in Progress for topology ID.*"), new Runnable() {
+         @Override
+         public void run() {
+            log.debug("Rehash started");
+            addEvent(hashChanges, true, 0, 0);
+         }
+      });
+      service.registerAction(Pattern.compile(".*Unlock State Transfer in Progress for topology ID.*"), new Runnable() {
+         @Override
+         public void run() {
+            log.debug("Rehash finished");
+            addEvent(hashChanges, false, 0, 0);
+         }
+      });
+   }
+}
