99e54ba76b0bd819ff211b9ede44436cd6a9e4b5
Alan Field <afield@redhat.com>
2014-06-27 15:35:30 +0200

    Fix cachestore dependencies

--- plugins/jdg63/src/main/java/org/radargun/service/JDG63EmbeddedService.java
+++ plugins/jdg63/src/main/java/org/radargun/service/JDG63EmbeddedService.java
@@ -1,5 +1,11 @@
 package org.radargun.service;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import org.infinispan.commons.util.FileLookup;
+import org.infinispan.configuration.parsing.ConfigurationBuilderHolder;
+import org.infinispan.configuration.parsing.ParserRegistry;
 import org.radargun.Service;
 import org.radargun.traits.ProvidesTrait;
 
@@ -11,16 +17,26 @@ public class JDG63EmbeddedService extends Infinispan60EmbeddedService {
    protected static final String SERVICE_DESCRIPTION = "Service hosting JDG in embedded (library) mode.";
 
    @Override
+   protected ConfigurationBuilderHolder createConfiguration(String configFile) throws FileNotFoundException {
+      try {
+         InputStream input = new FileLookup().lookupFileStrict(configFile, Thread.currentThread()
+               .getContextClassLoader());
+         return new ParserRegistry(Thread.currentThread().getContextClassLoader()).parse(input);
+      } catch (Exception e) {
+         throw new RuntimeException(e);
+      }
+   }
+
+   @Override
    @ProvidesTrait
    public Infinispan70MapReduce createMapReduce() {
       return new Infinispan70MapReduce(this);
    }
 
-// TODO: Add this when the next ER is released
-//   @Override
-//   @ProvidesTrait
-//   public InfinispanCacheInfo createCacheInformation() {
-//      return new Infinispan70CacheInfo(this);
-//   }
+   @Override
+   @ProvidesTrait
+   public InfinispanCacheInfo createCacheInformation() {
+      return new Infinispan70CacheInfo(this);
+   }
 
 }
