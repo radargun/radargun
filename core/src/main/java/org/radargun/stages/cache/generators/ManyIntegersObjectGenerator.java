b743bfa01f497116bd49e92be3fa40201091e738
rvansa <rvansa@redhat.com>
2014-08-20 10:14:06 +0200

    Added queryable ManyIntegersObject (+generator)

---
+++ core/src/main/java/org/radargun/stages/cache/generators/ManyIntegersObjectGenerator.java
@@ -0,0 +1,87 @@
+package org.radargun.stages.cache.generators;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Random;
+
+import org.radargun.config.Property;
+import org.radargun.config.PropertyHelper;
+import org.radargun.utils.Utils;
+
+/**
+ * Generates the {@link org.radargun.query.ManyIntegersObject} instances
+ *
+ * @author Radim Vansa &lt;rvansa@redhat.com&gt;
+ */
+public class ManyIntegersObjectGenerator implements ValueGenerator {
+
+   @Property(doc = "Minimum value for all numbers (inclusive). Default is Integer.MIN_VALUE")
+   private int min = Integer.MIN_VALUE;
+
+   @Property(doc = "Maximum value for all numbers (inclusive). Default is Integer.MAX_VALUE")
+   private int max = Integer.MAX_VALUE;
+
+   @Property(name = "class", doc = "Class instantiated by this generator. Default is 'org.radargun.query.ManyIntegersObject'.")
+   private String clazzName = "org.radargun.query.ManyIntegersObject";
+
+   @Property(doc = "Expected number of integers in this object. Default is 10.")
+   private int numInts = 10;
+
+   private Class<?> clazz;
+   private Constructor<?> ctor;
+   private Field[] fields;
+
+   @Override
+   public void init(String param, ClassLoader classLoader) {
+      PropertyHelper.setProperties(this, Utils.parseParams(param), false, false);
+      if (max > min) throw new IllegalArgumentException(String.format("min (%d) must be <= max (%d)", min, max));
+      try {
+         clazz = classLoader.loadClass(this.clazzName);
+         Class<?>[] params = new Class<?>[numInts];
+         fields = new Field[numInts];
+         for (int i = 0; i < params.length; ++i) {
+            params[i] = int.class;
+            fields[i] = clazz.getDeclaredField("int" + i);
+            fields[i].setAccessible(true);
+         }
+         ctor = clazz.getConstructor(params);
+      } catch (Exception e) {
+         throw new IllegalArgumentException(e);
+      }
+   }
+
+   @Override
+   public Object generateValue(Object key, int size, Random random) {
+      Object[] params = new Object[numInts];
+      for (int i = 0; i < params.length; ++i) params[i] = randomInt(random);
+      try {
+         return ctor.newInstance(params);
+      } catch (Exception e) {
+         throw new IllegalStateException(e);
+      }
+   }
+
+   private int randomInt(Random random) {
+      long l = random.nextLong();
+      return  max > min ? (int)((l < 0 ? ~l : l) % (max - min + 1) + min) : 0;
+   }
+
+   @Override
+   public int sizeOf(Object value) {
+      return -1;
+   }
+
+   @Override
+   public boolean checkValue(Object value, int expectedSize) {
+      if (!clazz.isInstance(value)) return false;
+      try {
+         for (Field f : fields) {
+            int v = f.getInt(value);
+            if (v < min || v > max) return false;
+         }
+         return true;
+      } catch (IllegalAccessException e) {
+         throw new IllegalStateException(e);
+      }
+   }
+}
