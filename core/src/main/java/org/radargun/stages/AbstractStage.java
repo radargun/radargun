31928b957a2a75a72cfaf74dca756e6ecca5c1a2
Alan Field <afield@redhat.com>
2014-08-20 17:05:01 +0200

    Add stage property to exit the benchmark if a stage fails

--- core/src/main/java/org/radargun/stages/AbstractStage.java
+++ core/src/main/java/org/radargun/stages/AbstractStage.java
@@ -1,5 +1,6 @@
 package org.radargun.stages;
 
+import org.radargun.config.Property;
 import org.radargun.config.Stage;
 import org.radargun.config.StageHelper;
 import org.radargun.logging.Log;
@@ -15,8 +16,17 @@ public abstract class AbstractStage implements org.radargun.Stage {
 
    protected Log log = LogFactory.getLog(getClass());
 
+   @Property(doc = "If true, then the benchmark stops when the stage returns an "
+         + "error. If false, then the stages in the current scenario are skipped, "
+         + "and the next scenario starts executing. Default is false.")
+   protected boolean exitOnFailure = false;
+
    public String getName() {
       return StageHelper.getStageName(getClass());
+   }
+
+   public boolean isExitOnFailure() {
+      return exitOnFailure;
    }
 
    @Override
