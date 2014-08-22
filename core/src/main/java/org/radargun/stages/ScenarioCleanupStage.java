a4da9581e7d6ecab70b05659c8fec08f9c70d18a
rvansa <rvansa@redhat.com>
2014-08-22 08:39:32 +0200

    Added thread leak detection support (and fixed threads leaking from Infinispan)

--- core/src/main/java/org/radargun/stages/ScenarioCleanupStage.java
+++ core/src/main/java/org/radargun/stages/ScenarioCleanupStage.java
@@ -1,7 +1,9 @@
 package org.radargun.stages;
 
 import java.io.File;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.radargun.DistStageAck;
 import org.radargun.config.Property;
@@ -19,8 +21,8 @@ import org.radargun.utils.Utils;
 @Stage(internal = true, doc = "DO NOT USE DIRECTLY. This stage is automatically inserted after the last stage in each scenario. You can alter the properties in &lt;cleanup/&gt element.")
 public class ScenarioCleanupStage extends AbstractDistStage {
 
-   @Property(doc = "Specifies whether the check for amount of free memory should be performed. Default is true.")
-   private boolean checkMemoryReleased = true;
+   @Property(doc = "Specifies whether the check for amount of free memory should be performed. Default is true.", deprecatedName = "checkMemoryReleased")
+   private boolean checkMemory = true;
 
    @Property(doc = "If the free memory after wrapper destroy and System.gc() is below percentage specified in this property the benchmark will stop. Default is 95.")
    private byte memoryThreshold = 95;
@@ -28,43 +30,49 @@ public class ScenarioCleanupStage extends AbstractDistStage {
    @Property(doc = "Directory where the heap dump will be produced if the memory threshold is hit. By default the dump will not be produced.")
    private String heapDumpDir = null;
 
+   @Property(doc = "Specifies whether the check for unfinished threads should be performed. Default is true.")
+   private boolean checkThreads = true;
+
    public DistStageAck executeOnSlave() {
       log.info("Scenario finished, running cleanup...");
       try {
          if (lifecycle != null && lifecycle.isRunning()) {
             LifecycleHelper.stop(slaveState, true, false);
-            //reset the class loader to SystemClassLoader
-            Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
             log.info("Service successfully stopped.");
          } else {
             log.info("No service deployed on this slave, nothing to do.");
-            return new MemoryAck(slaveState, Runtime.getRuntime().freeMemory());
          }
       } catch (Exception e) {
          return errorResponse("Problems shutting down the slave", e);
       } finally {
-         log.info(Utils.printMemoryFootprint(true));
-         if (checkMemoryReleased) {
-            checkMemoryReleased();
-         } else {
-            System.gc();
-         }
-         log.info(Utils.printMemoryFootprint(false));
+         //reset the class loader to SystemClassLoader
+         Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
+         slaveState.resetClassLoader();
       }
-      return new MemoryAck(slaveState, Runtime.getRuntime().freeMemory());
-   }
 
+      log.info(Utils.printMemoryFootprint(true));
+      if (checkMemory) {
+         checkMemoryReleased();
+      } else {
+         System.gc();
+      }
+      log.info(Utils.printMemoryFootprint(false));
+
+      int unfinishedThreads = 0;
+      if (checkThreads) {
+         unfinishedThreads = checkThreadsFinished();
+      }
+      return new CleanupAck(slaveState, (Long) slaveState.get(ScenarioInitStage.INITIAL_FREE_MEMORY), Runtime.getRuntime().freeMemory(), unfinishedThreads);
+   }
 
    @Override
    public boolean processAckOnMaster(List<DistStageAck> acks) {
-      boolean result = super.processAckOnMaster(acks);
-      if (result && masterState.get(ScenarioInitStage.INITIAL_FREE_MEMORY) == null) {
-         masterState.put(ScenarioInitStage.INITIAL_FREE_MEMORY, "");
-         for (MemoryAck ack : Projections.instancesOf(acks, MemoryAck.class)) {
-            String key = ScenarioInitStage.INITIAL_FREE_MEMORY + "_" + ack.getSlaveIndex();
-            log.info(String.format("Node %d has final free memory of: %d kb", ack.getSlaveIndex(), ack.freeMemory / 1024));
-            masterState.put(key, ack.freeMemory);
-         }
+      if (!super.processAckOnMaster(acks)) return false;
+      boolean result = true;
+      for (CleanupAck ack : Projections.instancesOf(acks, CleanupAck.class)) {
+         log.info(String.format("Node %d has changed free memory from %d MB to %d MB and has %d unfinished threads",
+               ack.getSlaveIndex(), ack.initialFreeMemory / 1048576, ack.finalFreeMemory / 1048576, ack.unfinishedThreads));
+         result = result && ack.unfinishedThreads == 0;
       }
       return result;
    }
@@ -95,12 +103,36 @@ public class ScenarioCleanupStage extends AbstractDistStage {
       }
    }
 
-   private static class MemoryAck extends DistStageAck {
-      final long freeMemory;
+   private int checkThreadsFinished() {
+      Thread[] activeThreads = new Thread[Thread.activeCount() * 2];
+      int activeCount = Thread.enumerate(activeThreads);
+      Set<Thread> threads = new HashSet<>(activeCount);
+      for (int i = 0; i < activeCount; ++i) threads.add(activeThreads[i]);
+      Set<Thread> initialThreads = (Set<Thread>) slaveState.get(ScenarioInitStage.INITIAL_THREADS);
+      threads.removeAll(initialThreads);
+      for (Thread thread : threads) {
+         StringBuilder sb = new StringBuilder();
+         sb.append("Unfinished thread ").append(thread.getName()).append(" (id=")
+               .append(thread.getId()).append(", status=").append(thread.getState()).append(")");
+         for (StackTraceElement ste : thread.getStackTrace()) {
+            sb.append("\n\tat ");
+            sb.append(ste.toString());
+         }
+         log.warn(sb.toString());
+      }
+      return threads.size();
+   }
 
-      private MemoryAck(SlaveState slaveState, long freeMemory) {
+
+   private static class CleanupAck extends DistStageAck {
+      private final long initialFreeMemory, finalFreeMemory;
+      private final int unfinishedThreads;
+
+      private CleanupAck(SlaveState slaveState, long initialFreeMemory, long finalFreeMemory, int unfinishedThreads) {
          super(slaveState);
-         this.freeMemory = freeMemory;
+         this.initialFreeMemory = initialFreeMemory;
+         this.finalFreeMemory = finalFreeMemory;
+         this.unfinishedThreads = unfinishedThreads;
       }
    }
 }
