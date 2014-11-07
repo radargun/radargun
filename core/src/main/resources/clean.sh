2813aacf76c15eb5cf975f37e2d3f640e5d0edcc
Matej Cimbora <mcimbora@redhat.com>
2014-11-07 08:54:00 +0100

    Extended clean.sh script to kill running RG processes

--- core/src/main/resources/clean.sh
+++ core/src/main/resources/clean.sh
@@ -4,7 +4,10 @@
 if [ "x$RADARGUN_HOME" = "x" ]; then DIRNAME=`dirname $0`; RADARGUN_HOME=`cd $DIRNAME/..; pwd` ; fi; export RADARGUN_HOME
 . ${RADARGUN_HOME}/bin/includes.sh
 
+kill -9 `jps | grep 'Slave\|LaunchMaster' | cut -f "1" -d " "` > /dev/null
+
 rm *.out > /dev/null
 rm *.log > /dev/null
+rm *.pid > /dev/null
 rm -rf results > /dev/null
 
