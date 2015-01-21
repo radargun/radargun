8bdb5e284590596ca0d017d14a7567401d99a808
rvansa <rvansa@redhat.com>
2015-01-21 16:12:44 +0100

    DistributedTaskExecutor trait changed interface to Builder + Task style

--- core/src/main/java/org/radargun/traits/DistributedTaskExecutor.java
+++ core/src/main/java/org/radargun/traits/DistributedTaskExecutor.java
@@ -1,36 +1,50 @@
 package org.radargun.traits;
 
 import java.util.List;
-import java.util.Map;
+import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 
 @Trait(doc = "Provides the interface for executing distributed tasks.")
 public interface DistributedTaskExecutor<T> {
 
-   /**
-    * 
-    * This method executes the specified Distributed task against all of the keys in the cache.
-    * 
-    * @param distributedCallableFqn
-    *           The fully qualified class name for the org.infinispan.distexec.DistributedCallable
-    *           or java.util.concurrent.Callable implementation. The implementation must have a no
-    *           argument constructor. This class name is required.
-    *
-    * @param executionPolicyName
-    *           The name of one of the org.infinispan.distexec.DistributedTaskExecutionPolicy enums
-    *           or <code>null</code>
-    *
-    * @param failoverPolicyFqn
-    *           The fully qualified class name for a
-    *           org.infinispan.distexec.DistributedTaskFailoverPolicy implementation or
-    *           <code>null</code>
-    *
-    * @param nodeAddress
-    *
-    * @param params
-    *
-    * @return
-    */
-   public List<Future<T>> executeDistributedTask(String distributedCallableFqn,
-                                                 String executionPolicyName, String failoverPolicyFqn, String nodeAddress, Map<String, String> params);
+   interface Builder<T> {
+      /**
+       * @param callable Distributed task that should be executed.
+       * @return This builder instance.
+       */
+      Builder callable(Callable<T> callable);
+
+      /**
+       * @param executionPolicy Custom argument for task execution.
+       * @return This builder instance.
+       */
+      Builder executionPolicy(String executionPolicy);
+
+      /**
+       * @param failoverPolicy Custom argument for failover.
+       * @return This builder instance.
+       */
+      Builder failoverPolicy(String failoverPolicy);
+
+      /**
+       * // TODO we should rather set list of slave ids where this should be executed
+       * @param nodeAddress Describes target node in plugin-specific way.
+       * @return
+       */
+      Builder nodeAddress(String nodeAddress);
+
+      /**
+       * @return Task to be executed
+       */
+      Task build();
+   }
+
+   interface Task<T> {
+      /**
+       * Start task execution and return futures for completion of the task.
+       */
+      List<Future<T>> execute();
+   }
+
+   Builder builder(String cacheName);
 }
