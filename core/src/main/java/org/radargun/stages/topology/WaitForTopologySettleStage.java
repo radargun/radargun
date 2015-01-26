fd56a53176e0855d77937a9cac7a02e1f0a2b3ff
rvansa <rvansa@redhat.com>
2015-01-26 10:55:51 +0100

    WaitForTopologySettleStage also waits for calm period in membership history

--- core/src/main/java/org/radargun/stages/topology/WaitForTopologySettleStage.java
+++ core/src/main/java/org/radargun/stages/topology/WaitForTopologySettleStage.java
@@ -5,12 +5,13 @@ import java.util.List;
 import org.radargun.DistStageAck;
 import org.radargun.config.Property;
 import org.radargun.config.Stage;
-import org.radargun.utils.TimeConverter;
 import org.radargun.stages.AbstractDistStage;
+import org.radargun.traits.Clustered;
 import org.radargun.traits.InjectTrait;
 import org.radargun.traits.TopologyHistory;
+import org.radargun.utils.TimeConverter;
 
-@Stage(doc = "Waits for a period without any change in topology history.")
+@Stage(doc = "Waits for a period without any change in membership/topology history.")
 public class WaitForTopologySettleStage extends AbstractDistStage {
 
    @Property(doc = "How long period without any change are we looking for. Default is 10 seconds.", converter = TimeConverter.class)
@@ -19,11 +20,23 @@ public class WaitForTopologySettleStage extends AbstractDistStage {
    @Property(doc = "How long should we wait until we give up with error, 0 means indefinitely. Default is 10 minutes.", converter = TimeConverter.class)
    private long timeout = 600000;
 
-   @InjectTrait(dependency = InjectTrait.Dependency.MANDATORY)
-   TopologyHistory history;
-
    @Property(doc = "Name of the cache where we detect the events. Default is the default cache.")
    private String cacheName;
+
+   @Property(doc = "Wait for cache topology settle. Default is true.")
+   private boolean checkTopology = true;
+
+   @Property(doc = "Wait for data rehashes to settle. Default is true.")
+   private boolean checkDataRehash = true;
+
+   @Property(doc = "Wait for cluster membership to settle. Default is true (if the Clustered trait is supported).")
+   private boolean checkMembership = true;
+
+   @InjectTrait(dependency = InjectTrait.Dependency.MANDATORY)
+   private TopologyHistory history;
+
+   @InjectTrait
+   private Clustered clustered;
 
    @Override
    public DistStageAck executeOnSlave() {
@@ -40,7 +53,8 @@ public class WaitForTopologySettleStage extends AbstractDistStage {
 
          List<TopologyHistory.Event> topologyChangeHistory = history.getTopologyChangeHistory(cacheName);
          List<TopologyHistory.Event> rehashHistory = history.getRehashHistory(cacheName);
-         if (topologyChangeHistory != null && !topologyChangeHistory.isEmpty()) {
+         List<Clustered.Membership> membershipHistory = clustered == null ? null : clustered.getMembershipHistory();
+         if (checkTopology && topologyChangeHistory != null && !topologyChangeHistory.isEmpty()) {
             TopologyHistory.Event event = topologyChangeHistory.get(topologyChangeHistory.size() - 1);
             if (event.getEnded() == null) {
                log.debug("Topology change event has not finished: " + event);
@@ -50,13 +64,20 @@ public class WaitForTopologySettleStage extends AbstractDistStage {
                settled = false;
             }
          }
-         if (rehashHistory != null && !rehashHistory.isEmpty()) {
+         if (checkDataRehash && rehashHistory != null && !rehashHistory.isEmpty()) {
             TopologyHistory.Event event = rehashHistory.get(rehashHistory.size() - 1);
             if (event.getEnded() == null) {
                log.debug("Rehash event has not finished: " + event);
                settled = false;
             } else if (now < event.getEnded().getTime() + period) {
                log.debug("Last rehash event finished too recently: " + event);
+               settled = false;
+            }
+         }
+         if (checkMembership && membershipHistory != null && !membershipHistory.isEmpty()) {
+            Clustered.Membership membership = membershipHistory.get(membershipHistory.size() - 1);
+            if (now < membership.date.getTime() + period) {
+               log.debug("Last membership changed too recently: " + membership);
                settled = false;
             }
          }
