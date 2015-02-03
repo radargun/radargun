a4d00849d5db195d928fd43ba90f35e03ce54bdb
rvansa <rvansa@redhat.com>
2015-02-03 12:53:10 +0100

    RadarGun 2.0.0.Final

--- core/src/main/java/org/radargun/Version.java
+++ core/src/main/java/org/radargun/Version.java
@@ -1,7 +1,7 @@
 package org.radargun;
 
 public class Version {
-   private static final String version = "2.0.0-SNAPSHOT";
+   private static final String version = "2.0.0.Final";
    public static void main(String[] args) {
       System.out.println("=== RadarGun ===");
       System.out.println("Version: " + version);
