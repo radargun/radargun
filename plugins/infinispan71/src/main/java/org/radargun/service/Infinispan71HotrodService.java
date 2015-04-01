8e99dd319078969f5a9cb09db506ad6301b5f84d
Vojtech Juranek <vjuranek@redhat.com>
2015-04-01 11:26:58 +0200

    Add support for near caching

---
+++ plugins/infinispan71/src/main/java/org/radargun/service/Infinispan71HotrodService.java
@@ -0,0 +1,32 @@
+package org.radargun.service;
+
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.client.hotrod.configuration.NearCacheMode;
+import org.radargun.Service;
+import org.radargun.config.Property;
+import org.radargun.config.PropertyDelegate;
+
+/**
+ * @author Vojtech Juranek &lt;vjuranek@redhat.com&gt;
+ */
+@Service(doc = Infinispan60HotrodService.SERVICE_DESCRIPTION)
+public class Infinispan71HotrodService extends Infinispan70HotrodService {
+
+   @PropertyDelegate(prefix = "nearCaching.")
+   private NearCaching nearCachingConfig = new NearCaching();
+   
+   protected ConfigurationBuilder getDefaultHotRodConfig() {
+      ConfigurationBuilder cb = super.getDefaultHotRodConfig();
+      cb.nearCache().mode(nearCachingConfig.mode).maxEntries(nearCachingConfig.maxEntries);
+      return cb;
+   }
+   
+   public static class NearCaching {
+      @Property(doc = "Near caching mode. Default is DISABLED.")
+      protected NearCacheMode mode = NearCacheMode.DISABLED;
+      
+      @Property(doc = "Maximum number or entires in near cache")
+      protected int maxEntries = -1;
+   }
+
+}
