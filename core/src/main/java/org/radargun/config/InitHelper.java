13ec0b108240c5e28e138e368dec2febffc5e714
rvansa <rvansa@redhat.com>
2015-04-09 19:00:06 +0200

    Ignore overridden @Init method - execute only once

--- core/src/main/java/org/radargun/config/InitHelper.java
+++ core/src/main/java/org/radargun/config/InitHelper.java
@@ -1,12 +1,18 @@
 package org.radargun.config;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Stack;
+
+import org.radargun.logging.Log;
+import org.radargun.logging.LogFactory;
 
 /**
  * @author Radim Vansa &lt;rvansa@redhat.com&gt;
  */
 public class InitHelper {
+   private static final Log log = LogFactory.getLog(InitHelper.class);
+
    public static void init(Object target) {
       if (target == null) throw new NullPointerException();
       Stack<Method> inits = new Stack<Method>();
@@ -14,7 +20,17 @@ public class InitHelper {
       while (clazz != null) {
          for (Method m : clazz.getDeclaredMethods()) {
             if (m.getAnnotation(Init.class) != null) {
-               inits.push(m);
+               boolean overridden = false;
+               for (Method m2 : inits) {
+                  if (m2.getName().equals(m.getName())
+                        && Arrays.equals(m2.getGenericParameterTypes(), m.getGenericParameterTypes())) {
+                     log.warnf("Method %s overrides %s but both are declared with @Init annotation: calling only once", m2, m);
+                     overridden = true;
+                  }
+               }
+               if (!overridden) {
+                  inits.push(m);
+               }
             }
          }
          clazz = clazz.getSuperclass();
