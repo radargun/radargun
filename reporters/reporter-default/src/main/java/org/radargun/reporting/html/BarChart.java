12b4b319971d12e0044be89435d1140a3c2975ff
rvansa <rvansa@redhat.com>
2015-04-14 11:40:34 +0200

    In charts, large deviation should not scale the charts so much that the mean wouldn't be seen

--- reporters/reporter-default/src/main/java/org/radargun/reporting/html/BarChart.java
+++ reporters/reporter-default/src/main/java/org/radargun/reporting/html/BarChart.java
@@ -15,6 +15,7 @@ public class BarChart extends ComparisonChart {
 
    protected final DefaultStatisticalCategoryDataset categorySet = new DefaultStatisticalCategoryDataset();
    protected boolean hasDeviations = false;
+   protected double upperRange = 0;
 
    public BarChart(String domainLabel, String rangeLabel) {
       super(domainLabel, rangeLabel);
@@ -27,12 +28,16 @@ public class BarChart extends ComparisonChart {
       if (hasDeviations) {
          chart.getCategoryPlot().setRenderer(new StatisticalBarRenderer());
       }
+      if (upperRange > 0) {
+         chart.getCategoryPlot().getRangeAxis().setRange(0, upperRange * 1.1);
+      }
       return chart;
    }
 
    @Override
    public void addValue(double value, double deviation, Comparable seriesName, double xValue, String xString) {
       hasDeviations = deviation != 0;
+      upperRange = Math.max(upperRange, Math.min(value + deviation, 3 * value));
       categorySet.add(value, deviation, seriesName, xString);
    }
 }
