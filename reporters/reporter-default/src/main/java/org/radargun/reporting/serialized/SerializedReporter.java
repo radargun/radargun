0b2c47bbd6f83f5cd148d515ddc4b9c32c91432c
Matej Cimbora <mcimbora@redhat.com>
2015-08-25 13:21:50 +0200

    Serialized reporter - Allow specifying multiple result dir source paths

--- reporters/reporter-default/src/main/java/org/radargun/reporting/serialized/SerializedReporter.java
+++ reporters/reporter-default/src/main/java/org/radargun/reporting/serialized/SerializedReporter.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 import org.radargun.ShutDownHook;
 import org.radargun.config.DomConfigParser;
+import org.radargun.config.InitHelper;
 import org.radargun.config.MasterConfig;
 import org.radargun.config.Property;
 import org.radargun.config.ReporterConfiguration;
@@ -57,15 +58,26 @@ public class SerializedReporter implements Reporter {
 
    public static void main(String args[]) {
       if (args.length < 2) {
-         System.err.println("java " + SerializedReporter.class.getName() + " benchmark.xml /path/to/target-dir [reporter-dir...]");
+         System.err.println("java " + SerializedReporter.class.getName() + " benchmark.xml [--add-result-dir=/path/to/target-dir]+ [--add-reporter-dir=/path/to/reporter-dir]*");
          return;
       }
-      String benchmark = args[0];
-      String targetDir = args[1];
-      for (int i = 2; i < args.length; ++i) {
-         ReporterHelper.registerReporters(args[i]);
+
+      List<Report> reports = new ArrayList<>();
+      for (int i = 1; i < args.length; ++i) {
+         String arg = args[i];
+         if (arg.matches("--add-result-dir=.+")) {
+            addReport(reports, arg.substring(arg.indexOf("=") + 1));
+         } else if (arg.matches("--add-reporter-dir=.+")) {
+            ReporterHelper.registerReporters(arg.substring(arg.indexOf("=") + 1));
+         } else {
+            throw new IllegalArgumentException("Parameter " + arg + " not recognized");
+         }
+      }
+      if (reports.isEmpty()) {
+         throw new IllegalArgumentException("No reports have been initialized. Check whether correct result directory paths were provided");
       }
 
+      String benchmark = args[0];
       MasterConfig config;
       try {
          config = DomConfigParser.getConfigParser().parseConfig(benchmark);
@@ -75,8 +87,29 @@ public class SerializedReporter implements Reporter {
          return;
       }
 
-      List<Report> reports = new ArrayList<>();
-      for (File reportFile : new File(targetDir).listFiles(new FileFilter() {
+      for (ReporterConfiguration rc : config.getReporters()) {
+         for (ReporterConfiguration.Report rcr : rc.getReports()) {
+            Reporter reporter = null;
+            try {
+               reporter = ReporterHelper.createReporter(rc.type, rcr.getProperties());
+               if (reporter instanceof SerializedReporter) continue;
+               reporter.run(reports);
+            } catch (Exception e) {
+               System.err.println("Failed to run reporter " + rc.type);
+               e.printStackTrace();
+            } finally {
+               if (reporter != null) {
+                  InitHelper.destroy(reporter);
+               }
+            }
+         }
+      }
+
+      ShutDownHook.exit(0); // the shutdown is controlled
+   }
+
+   private static void addReport(List<Report> reports, String resultDir) {
+      for (File reportFile : new File(resultDir).listFiles(new FileFilter() {
          @Override
          public boolean accept(File pathname) {
             return pathname.getName().toLowerCase().endsWith(FILE_EXTENSION) && !pathname.isDirectory();
@@ -97,20 +130,5 @@ public class SerializedReporter implements Reporter {
             e.printStackTrace();
          }
       }
-
-      for (ReporterConfiguration rc : config.getReporters()) {
-         for (ReporterConfiguration.Report rcr : rc.getReports()) {
-            try {
-               Reporter reporter = ReporterHelper.createReporter(rc.type, rcr.getProperties());
-               if (reporter instanceof SerializedReporter) continue;
-               reporter.run(reports);
-            } catch (Exception e) {
-               System.err.println("Failed to run reporter " + rc.type);
-               e.printStackTrace();
-            }
-         }
-      }
-
-      ShutDownHook.exit(0); // the shutdown is controlled
    }
 }
