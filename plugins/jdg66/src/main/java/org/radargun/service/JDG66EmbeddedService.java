44a63af2ff7d45017363be893c56942c2ff47642
Vojtech Juranek <vjuranek@redhat.com>
2015-09-29 10:22:37 +0200

    Support continuous query in JDG 6.6

---
+++ plugins/jdg66/src/main/java/org/radargun/service/JDG66EmbeddedService.java
@@ -0,0 +1,20 @@
+package org.radargun.service;
+
+import org.radargun.Service;
+import org.radargun.traits.ProvidesTrait;
+
+/**
+ *
+ * @author vjuranek
+ *
+ */
+@Service(doc = JDG66EmbeddedService.SERVICE_DESCRIPTION)
+public class JDG66EmbeddedService extends JDG64EmbeddedService  {
+    protected static final String SERVICE_DESCRIPTION = "Service hosting JDG in embedded (library) mode.";
+
+    @ProvidesTrait
+    public InfinispanEmbeddedContinuousQuery createContinuousQuery() {
+        return new InfinispanEmbeddedContinuousQuery(this);
+    }
+
+}
