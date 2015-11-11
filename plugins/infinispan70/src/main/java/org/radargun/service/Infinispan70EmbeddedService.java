eeaa1645ffaf97ded20f3f966be27dffb54d3226
Matej Cimbora <mcimbora@redhat.com>
2015-11-11 13:08:50 +0100

    JGroups probe wrapper

--- plugins/infinispan70/src/main/java/org/radargun/service/Infinispan70EmbeddedService.java
+++ plugins/infinispan70/src/main/java/org/radargun/service/Infinispan70EmbeddedService.java
@@ -1,6 +1,9 @@
 package org.radargun.service;
 
+import org.infinispan.remoting.transport.jgroups.JGroupsTransport;
+import org.jgroups.protocols.TP;
 import org.radargun.Service;
+import org.radargun.config.Property;
 import org.radargun.traits.ProvidesTrait;
 
 /**
@@ -8,6 +11,28 @@ import org.radargun.traits.ProvidesTrait;
  */
 @Service(doc = InfinispanEmbeddedService.SERVICE_DESCRIPTION)
 public class Infinispan70EmbeddedService extends Infinispan60EmbeddedService {
+
+   @Property(doc = "Enable diagnostics port for service probing (true/false/null). Default is null - use settings from ISPN configuration.")
+   protected Boolean enableDiagnostics;
+
+   @Override
+   protected void startCaches() throws Exception {
+      super.startCaches();
+      setDiagnostics();
+   }
+
+   protected void setDiagnostics() {
+      JGroupsTransport transport = (JGroupsTransport) cacheManager.getTransport();
+      TP transportprotocol = (TP) transport.getChannel().getProtocolStack().findProtocol(TP.class);
+      if (Boolean.TRUE.equals(enableDiagnostics)) {
+         transportprotocol.enableDiagnostics();
+         log.debug("Enabling diagnostics in the transport protocol");
+      } else if (Boolean.FALSE.equals(enableDiagnostics)) {
+         transportprotocol.disableDiagnostics();
+         log.debug("Disabling diagnostics in the transport protocol");
+      }
+   }
+
    @SuppressWarnings("rawtypes")
    @Override
    @ProvidesTrait
