90da2f17b2c06465d35ab134ea3bb2413d78c561
Radim Vansa <rvansa@redhat.com>
2016-01-18 12:29:08 +0100

    Stage starting threads that just burn CPU time

--- core/src/main/java/org/radargun/stages/test/Blackhole.java
+++ core/src/main/java/org/radargun/stages/test/Blackhole.java
@@ -9,11 +9,24 @@ package org.radargun.stages.test;
 public final class Blackhole {
    // although counter is accessed from multiple threads, we don't care about synchronization
    private static long counter = 0;
+   private static volatile long consumedCPU;
 
    private Blackhole() {}
 
    public static void consume(Object object) {
       if (++counter != 0) return;
       if (object != null && System.identityHashCode(object) == object.hashCode()) System.out.print("");
+   }
+
+   public static void consumeCpu() {
+      long t = consumedCPU;
+
+      for(long i = 100; i > 0L; --i) {
+         t += t * 25214903917L + 11L + i & 281474976710655L;
+      }
+
+      if(t == 42L) {
+         consumedCPU += t;
+      }
    }
 }
