715a453ef42ef852408ae11af26ca8bbf00ec64a
Alan Field <afield@redhat.com>
2016-04-13 14:33:32 -0400

    Update Process plugin isRunning check
    Service process is running as long as Process.isAlive() returns true, or
    as long as any child process spawned by the serice is still running.
    Remove echo statements from unix-kill.sh and unix-stop.sh

--- plugins/process/src/main/resources/unix-kill.sh
+++ plugins/process/src/main/resources/unix-kill.sh
@@ -2,7 +2,6 @@
 source $(dirname $0)/unix-func.sh
 #findpids $1
 PIDS=$1
-echo "unix-kill: PIDS=$PIDS"
 for pid in $PIDS; do
    killtree $pid KILL;
 done;
--- plugins/process/src/main/resources/unix-stop.sh
+++ plugins/process/src/main/resources/unix-stop.sh
@@ -2,7 +2,6 @@
 source $(dirname $0)/unix-func.sh
 #findpids $1
 PIDS=$1
-echo "unix-stop: PIDS=$PIDS"
 for pid in $PIDS; do
    killtree $pid TERM;
 done;
