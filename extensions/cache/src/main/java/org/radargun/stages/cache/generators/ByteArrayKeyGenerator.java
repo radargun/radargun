5a06cd2a62fe7114300eff4a7b8fd186913bff60
rmacor <rmacor@redhat.com>
2016-09-22 11:47:00 +0200

    ByteArrayKeyGenerator

---
+++ extensions/cache/src/main/java/org/radargun/stages/cache/generators/ByteArrayKeyGenerator.java
@@ -0,0 +1,18 @@
+package org.radargun.stages.cache.generators;
+
+import java.nio.ByteBuffer;
+import org.radargun.config.DefinitionElement;
+
+/**
+ * @author Roman Macor (rmacor@redhat.com)
+ */
+@DefinitionElement(name = "byte-array-key", doc = "Generates byte-array keys")
+public class ByteArrayKeyGenerator implements KeyGenerator {
+
+   @Override
+   public Object generateKey(long keyIndex) {
+      ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
+      buffer.putLong(keyIndex);
+      return buffer.array();
+   }
+}
