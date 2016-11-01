c33eb944b051d2df7f7425ba7910324f3f2ff9ec
Vojtech Juranek <vjuranek@redhat.com>
2016-11-01 09:27:45 +0100

    #430 Fix problem with PowerMock on abstract class

--- core/src/test/java/org/radargun/config/ConfigurationWithImportedScenarioFromAnotherBenchmarkTest.java
+++ core/src/test/java/org/radargun/config/ConfigurationWithImportedScenarioFromAnotherBenchmarkTest.java
@@ -1,10 +1,13 @@
 package org.radargun.config;
 
+import org.testng.annotations.Test;
+
 /**
  * Variation of AbstractConfigurationTest when scenario is imported from another benchmark file
  *
  * @author Roman Macor &lt;rmacor@redhat.com&gt;
  */
+@Test
 public class ConfigurationWithImportedScenarioFromAnotherBenchmarkTest extends AbstractConfigurationTest {
    public ConfigurationWithImportedScenarioFromAnotherBenchmarkTest() {
       resources.add("benchmark-test.xml");
@@ -13,5 +16,35 @@ public class ConfigurationWithImportedScenarioFromAnotherBenchmarkTest extends A
    @Override
    protected String getBenchmark() {
       return "benchmark-importedScenarioFromAnotherBenchmark.xml";
+   }
+   
+   @Override //TODO remove once #431 is done
+   public void testClusters() {
+      super.testClusters();
+   }
+   
+   @Override //TODO remove once #431 is done
+   public void testConfiguration() {
+      super.testConfiguration();
+   }
+   
+   @Override //TODO remove once #431 is done
+   public void testHost() {
+      super.testHost();
+   }
+   
+   @Override //TODO remove once #431 is done
+   public void testPlugins() {
+      super.testPlugins();
+   }
+   
+   @Override //TODO remove once #431 is done
+   public void testReport() {
+      super.testReport();
+   }
+   
+   @Override //TODO remove once #431 is done
+   public void testScenario() {
+      super.testScenario();
    }
 }
