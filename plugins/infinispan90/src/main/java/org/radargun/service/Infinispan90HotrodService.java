cad9ba653dc7a5905e68b25edfc0578cd04fe13c
rmacor <rmacor@redhat.com>
2016-12-09 13:39:07 +0100

    Add option to specify Kryo and Protostuff marshallers

--- plugins/infinispan90/src/main/java/org/radargun/service/Infinispan90HotrodService.java
+++ plugins/infinispan90/src/main/java/org/radargun/service/Infinispan90HotrodService.java
@@ -1,6 +1,8 @@
 package org.radargun.service;
 
+import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 import org.radargun.Service;
+import org.radargun.config.Property;
 import org.radargun.traits.ContinuousQuery;
 import org.radargun.traits.ProvidesTrait;
 
@@ -10,9 +12,23 @@ import org.radargun.traits.ProvidesTrait;
 @Service(doc = Infinispan60HotrodService.SERVICE_DESCRIPTION)
 public class Infinispan90HotrodService extends Infinispan81HotrodService {
 
+   @Property(doc = "Fully qualified Marshaller class. If no class is defined, the default marshaller - JBossMarshaller - is used. " +
+           "Valid options are org.infinispan.marshaller.protostuff.ProtostuffMarshaller and org.infinispan.marshaller.kryo.KryoMarshaller")
+   protected String marshaller;
+
    @ProvidesTrait
    public ContinuousQuery createContinuousQuery() {
       return new Infinispan90HotrodContinuousQuery(this);
    }
 
+   @Override
+   protected ConfigurationBuilder getDefaultHotRodConfig() {
+      ConfigurationBuilder defaultHotRodConfig = super.getDefaultHotRodConfig();
+
+      if (marshaller != null) {
+         defaultHotRodConfig.marshaller(marshaller);
+         log.info("Marshaller is set to " + marshaller);
+      }
+      return defaultHotRodConfig;
+   }
 }
