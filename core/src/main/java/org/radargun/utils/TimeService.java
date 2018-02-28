161d8110f03b7c9abfdfcb3dd9151cb4cbd55b95
Martin Gencur <mgencur@redhat.com>
2018-02-28 15:38:58 +0100

    OpenShift plugin
    * deploying Pods/Service/etc. in OpenShift via template with parameters
    * can wait for Pods to be ready
    * can clean up the OpenShift project after test
    * stores information about pods/services in ServiceContext for later use

--- core/src/main/java/org/radargun/utils/TimeService.java
+++ core/src/main/java/org/radargun/utils/TimeService.java
@@ -1,5 +1,9 @@
 package org.radargun.utils;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+import java.util.function.BooleanSupplier;
+
 /**
  * Global service providing time operations.
  *
@@ -16,5 +20,30 @@ public final class TimeService {
 
    public static long nanoTime() {
       return System.nanoTime();
+   }
+
+   public static void waitFor(BooleanSupplier condition) {
+      TimeService.waitFor(condition, 60, TimeUnit.SECONDS, 1);
+   }
+
+   public static void waitFor(BooleanSupplier condition, long timeout, TimeUnit unit, int numberOfSuccessfulChecks) {
+      final int maxNumberOfBackofs = 100;
+      long endTime = TimeUnit.MILLISECONDS.convert(timeout, unit) + System.currentTimeMillis();
+      long backoffCounter = 0;
+      int successfulChecks = 0;
+      while (System.currentTimeMillis() - endTime < 0) {
+         if (condition.getAsBoolean()) {
+            if (++successfulChecks >= numberOfSuccessfulChecks) {
+               break;
+            }
+         } else {
+            successfulChecks = 0;
+         }
+         backoffCounter = backoffCounter + 1 > maxNumberOfBackofs ? maxNumberOfBackofs : backoffCounter + 1;
+         LockSupport.parkNanos(++backoffCounter * 100_000_000);
+      }
+      if (!condition.getAsBoolean()) {
+         throw new RuntimeException("Timed out waiting for a condition");
+      }
    }
 }
