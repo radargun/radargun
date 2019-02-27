b77056f061134f09ccebbc50fee2494c3366f33b
Diego Lovison <diegolovison@gmail.com>
2019-02-27 10:00:27 -0300

    Not supported plugin

--- plugins/infinispan52/src/main/java/org/radargun/service/Infinispan52CacheInfo.java
+++ plugins/infinispan52/src/main/java/org/radargun/service/Infinispan52CacheInfo.java
@@ -1,18 +1,13 @@
 package org.radargun.service;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 
 import org.infinispan.AdvancedCache;
 import org.infinispan.container.entries.InternalCacheEntry;
-import org.infinispan.distexec.DefaultExecutorService;
-import org.infinispan.distexec.DistributedExecutorService;
-import org.infinispan.distexec.DistributedTaskBuilder;
 import org.infinispan.distribution.DistributionManager;
 import org.infinispan.distribution.ch.ConsistentHash;
+import org.radargun.exception.PluginNotSupportedException;
 import org.radargun.logging.Log;
 import org.radargun.logging.LogFactory;
 import org.radargun.traits.CacheInformation;
@@ -58,40 +53,7 @@ public class Infinispan52CacheInfo extends InfinispanCacheInfo {
 
       @Override
       public long getTotalSize() {
-         long totalSize = 0;
-         DistributedExecutorService des = new DefaultExecutorService(cache);
-         CacheSizer<?, ?, Integer> cacheSizer = new CacheSizer<Object, Object, Integer>();
-         DistributedTaskBuilder<Integer> taskBuilder = des.createDistributedTaskBuilder(cacheSizer);
-         List<Future<Integer>> futureList = des.submitEverywhere(taskBuilder.build());
-
-         for (Future<Integer> future : futureList) {
-            try {
-               totalSize += future.get().intValue();
-            } catch (InterruptedException e) {
-               log.error("The distributed task was interrupted.", e);
-               return -1;
-            } catch (ExecutionException e) {
-               log.error("An error occurred executing the distributed task.", e);
-               return -1;
-            }
-         }
-
-         if (cache.getAdvancedCache().getDistributionManager() != null) {
-            int numMembers = cache.getCacheManager().getMembers().size();
-            int numOwners = cache.getAdvancedCache().getCacheConfiguration().clustering().hash().numOwners();
-            // In replicated mode, numOwners is always 2
-            if (cache.getAdvancedCache().getCacheConfiguration().clustering().cacheMode().isReplicated()) {
-               numOwners = numMembers;
-            }
-            // Adjust for current cluster size
-            if (numMembers >= numOwners) {
-               totalSize /= numOwners;
-            } else {
-               totalSize /= numMembers;
-            }
-         }
-
-         return totalSize;
+         throw new PluginNotSupportedException("DistributedExecutorService class was removed in Infinispan10");
       }
    }
 }
