6afb9c7b203df78a848641e733405655b918abb5
Diego Lovison <diegolovison@gmail.com>
2019-10-08 16:44:58 -0300

    Use infinispan-client-rest

--- plugins/infinispan100/src/main/java/org/radargun/service/InfinispanRestAPI.java
+++ plugins/infinispan100/src/main/java/org/radargun/service/InfinispanRestAPI.java
@@ -1,15 +1,15 @@
 package org.radargun.service;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 
+import org.infinispan.client.rest.RestResponse;
+import org.infinispan.client.rest.configuration.RestClientConfigurationBuilder;
+import org.infinispan.client.rest.impl.okhttp.RestClientOkHttp;
 import org.infinispan.server.network.NetworkAddress;
 import org.radargun.logging.Log;
 import org.radargun.logging.LogFactory;
@@ -24,20 +24,20 @@ public class InfinispanRestAPI {
 
    protected final Log log = LogFactory.getLog(getClass());
 
-   private ObjectMapper mapper;
-   private Integer defaultPort;
-   private static final String CACHE_MANAGER_RESOURCE = "v2/cache-managers/clustered";
-   private String serverIp;
+   private final ObjectMapper mapper;
+   private final RestClientOkHttp restClientOkHttp;
+   private final String cacheManagerName;
 
-   public InfinispanRestAPI(Integer defaultPort) {
+   public InfinispanRestAPI(Integer serverPort) {
       this.mapper = new ObjectMapper();
       this.mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
       this.mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
-      this.defaultPort = defaultPort;
-      this.serverIp = lookupServerIp();
+      this.cacheManagerName = System.getProperty("radargun.infinispan.cacheManagerName", "clustered");
+      this.restClientOkHttp = new RestClientOkHttp(new RestClientConfigurationBuilder()
+            .addServer().host(lookupServerHost()).port(serverPort).build());
    }
 
-   private String lookupServerIp() {
+   private String lookupServerHost() {
       // the following are predefined values in the INFINISPAN10 XML Files
       String interfaceName = System.getProperty("radargun.infinispan.interfaceName", "public");
       String bindAddress = System.getProperty("infinispan.bind.address", "127.0.0.1");
@@ -52,52 +52,15 @@ public class InfinispanRestAPI {
    }
 
    public CacheManagerInfo getCacheManager() {
-      String url = String.format("http://%s:%s/rest/%s", serverIp, this.defaultPort, CACHE_MANAGER_RESOURCE);
       CacheManagerInfo cacheManagerInfo = null;
       try {
-         String json = doGet(url);
-         cacheManagerInfo = mapper.readValue(json, CacheManagerInfo.class);
-      } catch (IOException e) {
-         log.error(String.format("Cannot access: %s -> %s", url, e.getMessage()));
+         RestResponse response = restClientOkHttp.cacheManager(this.cacheManagerName).info().toCompletableFuture()
+               .get();
+         cacheManagerInfo = mapper.readValue(response.getBodyAsStream(), CacheManagerInfo.class);
+      } catch (IOException | InterruptedException | ExecutionException e) {
+         log.error("Cannot access the cache manager: " + e.getMessage());
       }
       return cacheManagerInfo;
-   }
-
-   public static String doGet(String url) throws IOException {
-      return doGet(url, 200);
-   }
-
-   public static String doGet(String url, int expectedHttpStatusResponse) throws IOException {
-
-      StringBuilder content = new StringBuilder();
-      int httpResponseStatus = -1;
-      int count = 0;
-
-      // network is not reliable
-      while (httpResponseStatus != expectedHttpStatusResponse && count++ < 5) {
-         BufferedReader in = null;
-         content = new StringBuilder();
-         try {
-            HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
-            con.setRequestMethod("GET");
-            con.setConnectTimeout(5000);
-            con.setReadTimeout(5000);
-            int status = con.getResponseCode();
-            if (status != 200) {
-               content.append("ERROR: ").append(status).append("\n");
-            }
-            in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-               content.append(inputLine);
-            }
-         } finally {
-            if (in != null) {
-               in.close();
-            }
-         }
-      }
-      return content.toString();
    }
 
 }
