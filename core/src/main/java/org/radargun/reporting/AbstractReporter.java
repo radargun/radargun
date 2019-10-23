7de5139ed2bcb0d7993664bc74f505046450bdec
Diego Lovison <diegolovison@gmail.com>
2019-10-23 14:52:59 -0300

    Allow generate report based on master return code

--- core/src/main/java/org/radargun/reporting/AbstractReporter.java
+++ core/src/main/java/org/radargun/reporting/AbstractReporter.java
@@ -1,28 +1,42 @@
 package org.radargun.reporting;
 
 import java.util.Collection;
-import java.util.List;
 
 import org.radargun.config.MasterConfig;
 import org.radargun.config.Property;
 import org.radargun.logging.Log;
 import org.radargun.logging.LogFactory;
 
+/**
+ * Control the report generation based on the stage failure
+ *
+ * @author Diego Lovison &lt;dlovison@redhat.com&gt;
+ */
 public abstract class AbstractReporter implements Reporter {
 
    private static Log log = LogFactory.getLog(AbstractReporter.class);
 
-   @Property(doc = "Which master return code should make the report run. Default is all. Comma separated.")
-   private List<Integer> masterReturnCodes;
+   @Property(doc = "Set it to true if the report should be skipped when one stage failed. Default is false.")
+   private boolean skipOnStageFailures;
 
-   public void run(MasterConfig masterConfig, Collection<Report> reports, int masterReturnCode) {
+   public AbstractReporter() {
+   }
+
+   public AbstractReporter(boolean skipOnStageFailures) {
+      this.skipOnStageFailures = skipOnStageFailures;
+   }
+
+   @Override
+   public boolean run(MasterConfig masterConfig, Collection<Report> reports, int returnCode) {
       String className = this.getClass().getSimpleName();
-      if (masterReturnCodes == null || masterReturnCodes.isEmpty() || masterReturnCodes.contains(masterReturnCode)) {
+      boolean generateReport = skipOnStageFailures ? returnCode == 0 : true;
+      if (generateReport) {
          log.info("Running reporter " + className);
          run(masterConfig, reports);
+         return true;
       } else {
-         log.info(String.format("Skipping report %s. Return codes are %s and the master return code is %d",
-               className, masterReturnCodes, masterReturnCode));
+         log.info(String.format("Skipping report %s. Return code is %d", className, returnCode));
+         return false;
       }
    }
 
---
+++ core/src/test/java/org/radargun/reporting/AbstractReporterTest.java
@@ -0,0 +1,37 @@
+package org.radargun.reporting;
+
+import static org.testng.Assert.assertTrue;
+import static org.testng.AssertJUnit.assertFalse;
+
+import java.util.Collection;
+
+import org.radargun.config.MasterConfig;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Diego Lovison &lt;dlovison@redhat.com&gt;
+ */
+@Test
+public class AbstractReporterTest {
+
+   public void skipOnStageFailure() {
+      // default behavior, always generate
+      assertTrue(createDummyAbstractReporter(false).run(null, null, 0));
+      assertTrue(createDummyAbstractReporter(false).run(null, null, 1));
+
+      // configure to skip, return code is 0, then it will generate the report
+      assertTrue(createDummyAbstractReporter(true).run(null, null, 0));
+
+      // configure to skip, return code is a failure, then it will skip the report generation
+      assertFalse(createDummyAbstractReporter(true).run(null, null, 1));
+   }
+
+   private AbstractReporter createDummyAbstractReporter(boolean skipOnStageFailures) {
+      return new AbstractReporter(skipOnStageFailures) {
+         @Override
+         public void run(MasterConfig masterConfig, Collection<Report> reports) {
+         }
+      };
+   }
+}
