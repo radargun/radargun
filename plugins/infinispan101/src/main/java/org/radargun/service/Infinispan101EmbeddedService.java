56c76a2e7b5a27587b5f56abf993d035145db3ae
Diego Lovison <diegolovison@gmail.com>
2020-02-28 07:42:44 -0300

    Fix jmx method for ISPN 10x

--- plugins/infinispan101/src/main/java/org/radargun/service/Infinispan101EmbeddedService.java
+++ plugins/infinispan101/src/main/java/org/radargun/service/Infinispan101EmbeddedService.java
@@ -4,20 +4,20 @@ import org.radargun.Service;
 import org.radargun.traits.ProvidesTrait;
 
 @Service(doc = InfinispanEmbeddedService.SERVICE_DESCRIPTION)
-public class Infinispan110EmbeddedService extends Infinispan100EmbeddedService {
+public class Infinispan101EmbeddedService extends Infinispan100EmbeddedService {
 
    @ProvidesTrait
    public EmbeddedConfigurationProvider createConfigurationProvider() {
-      return new EmbeddedConfigurationProvider110(this);
+      return new EmbeddedConfigurationProvider101(this);
    }
 
    @Override
    protected String getJmxDomain() {
-      return JmxHelper110.getJmxDomain(cacheManager.getCacheManagerConfiguration());
+      return JmxHelper101.getJmxDomain(cacheManager.getCacheManagerConfiguration());
    }
 
    @Override
    protected ConfigDumpHelper createConfigDumpHelper() {
-      return new ConfigDumpHelper110();
+      return new ConfigDumpHelper101();
    }
 }
--- plugins/infinispan101/src/main/java/org/radargun/service/JmxHelper101.java
+++ plugins/infinispan101/src/main/java/org/radargun/service/JmxHelper101.java
@@ -2,13 +2,13 @@ package org.radargun.service;
 
 import org.infinispan.configuration.global.GlobalConfiguration;
 
-public class JmxHelper110 {
+public class JmxHelper101 {
 
-   private JmxHelper110() {
+   private JmxHelper101() {
 
    }
 
-   // TODO after 11.0.0.Alpha1 replace with jmx instead of globalJmxStatistics
+   // TODO after 11.0.0.Alpha1 replace with jmx instead of globalJmxStatistics.
    public static String getJmxDomain(GlobalConfiguration global) {
       return global.globalJmxStatistics().domain();
    }
