6593e628535ead9b5ea37a564741582fa1c7adae
Diego Lovison <diegolovison@gmail.com>
2022-10-27 14:09:53 -0300

    ISPN 13 with JGroups 5 code

--- ta/run-test.sh
+++ ta/run-test.sh
@@ -5,8 +5,11 @@ export JAVA_HOME=/Users/dlovison/.sdkman/candidates/java/current
 
 ./clean.sh
 ./main.sh -c example.xml
-./worker.sh -J "-Djava.net.preferIPv4Stack=true -agentpath:/Users/dlovison/Documents/GitHub/diego-kitchen/ispn/async-profiler-2.8.3-macos/build/libasyncProfiler.so=start,threads,event=cpu,file=profile1.html,include=org/jgroups/*"
-./worker.sh -J "-Djava.net.preferIPv4Stack=true -agentpath:/Users/dlovison/Documents/GitHub/diego-kitchen/ispn/async-profiler-2.8.3-macos/build/libasyncProfiler.so=start,threads,event=cpu,file=profile2.html,include=org/jgroups/*"
+# ./worker.sh -J "-Djava.net.preferIPv4Stack=true -agentpath:/Users/dlovison/Documents/GitHub/diego-kitchen/ispn/async-profiler-2.8.3-macos/build/libasyncProfiler.so=start,event=cpu,file=profile1-%p.html,include=org/jgroups/*"
+# ./worker.sh -J "-Djava.net.preferIPv4Stack=true -agentpath:/Users/dlovison/Documents/GitHub/diego-kitchen/ispn/async-profiler-2.8.3-macos/build/libasyncProfiler.so=start,event=cpu,file=profile2-%p.html,include=org/jgroups/*"
+
+./worker.sh -d 7778 --debug-suspend
+./worker.sh
 
 
 tail -f stdout_main.out
