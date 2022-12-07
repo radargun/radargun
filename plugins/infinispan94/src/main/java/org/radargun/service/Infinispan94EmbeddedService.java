7d30ad444a69064f973e07b07ffcf5c815943842
Diego Lovison <diegolovison@gmail.com>
2022-12-07 11:32:03 -0300

    Remove Infinispan 9.1, 9.2 and 9.3

--- plugins/infinispan94/src/main/java/org/radargun/service/Infinispan94EmbeddedService.java
+++ plugins/infinispan94/src/main/java/org/radargun/service/Infinispan94EmbeddedService.java
@@ -5,12 +5,12 @@ import org.radargun.traits.InternalsExposition;
 import org.radargun.traits.ProvidesTrait;
 
 /**
- * Infinispan 10.0.x Embedded Service
+ * Infinispan 9.4.x Embedded Service
  *
  * @author Diego Lovison &lt;dlovison@redhat.com&gt;
  */
 @Service(doc = InfinispanEmbeddedService.SERVICE_DESCRIPTION)
-public class Infinispan94EmbeddedService extends Infinispan92EmbeddedService {
+public class Infinispan94EmbeddedService extends Infinispan90EmbeddedService {
 
    @ProvidesTrait
    public InternalsExposition createInternalsExposition() {
